c80d96e3e64326117e77756535db97e82dfd5276
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-01-16 14:42:32 -0600

    Specify platform support: iOS 12, macOS 11

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Gravatar",
+    platforms: [.iOS(.v12), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
