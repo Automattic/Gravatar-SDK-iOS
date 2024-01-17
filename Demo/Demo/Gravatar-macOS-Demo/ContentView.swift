3e07b491ae1ca49ba4c1d46cf747eeb8fc3488f0
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-01-17 16:25:58 -0600

    Add Gravatar-macOS-Demo target to Demo project

---
+++ Demo/Demo/Gravatar-macOS-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gravatar-macOS-Demo
+//
+//  Created by Andrew Montgomery on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/Demo/Gravatar-macOS-Demo/Gravatar_macOS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gravatar_macOS_DemoApp.swift
+//  Gravatar-macOS-Demo
+//
+//  Created by Andrew Montgomery on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Gravatar_macOS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
