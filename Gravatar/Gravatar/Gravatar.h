11c0dff5f9b13131936c291a122786464e4ac64f
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-01-17 11:35:46 -0600

    Create Gravatar SDK project

---
+++ Gravatar/Gravatar/Gravatar.h
@@ -0,0 +1,18 @@
+//
+//  Gravatar.h
+//  Gravatar
+//
+//  Created by Andrew Montgomery on 1/17/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gravatar.
+FOUNDATION_EXPORT double GravatarVersionNumber;
+
+//! Project version string for Gravatar.
+FOUNDATION_EXPORT const unsigned char GravatarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gravatar/PublicHeader.h>
+
+
