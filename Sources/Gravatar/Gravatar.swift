43413b5860e7700282513da08c93ae173a7fd7fc
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-01-19 17:11:01 -0600

    Replace ReplaceMe.swift with placeholder Gravatar.swift

---
+++ Sources/Gravatar/Gravatar.swift
@@ -0,0 +1,28 @@
+import Foundation
+import CryptoKit
+import UIKit
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+public struct GravatarImage: View {
+
+    private let url: URL
+
+    public init(email: String) {
+        let hash = GravatarDataProvider.hash(email: "test@example.com")
+        self.url = URL(string: "https://gravatar.com/avatar/")!.appendingPathComponent(hash)
+    }
+
+    public var body: some View {
+        AsyncImage(url: self.url)
+    }
+}
+#endif
+
+struct GravatarDataProvider {
+    static func hash(email: String) -> String {
+        let hash = SHA256.hash(data: email.data(using: .utf8)!)
+        return hash.compactMap { String(format: "%02x", $0) }.joined()
+    }
+}
