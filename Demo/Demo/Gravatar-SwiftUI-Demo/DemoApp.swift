dff009f34d29863b14b8d7aa0d130df289b3f42b
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-01-19 16:51:39 -0600

    Create Gravatar-Demo project with a SwiftUI target

---
+++ Demo/Demo/Gravatar-SwiftUI-Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Andrew Montgomery on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
