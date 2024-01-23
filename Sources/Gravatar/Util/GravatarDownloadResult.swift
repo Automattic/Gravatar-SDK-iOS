d36ff141a62806da137f47162003f4fcc686b439
Pinar Olguc <pinarolguc@gmail.com>
2024-01-23 15:32:05 +0300

    Small update

--- Sources/Gravatar/Util/GravatarDownloadResult.swift
+++ Sources/Gravatar/Util/GravatarDownloadResult.swift
@@ -8,8 +8,6 @@
 import Foundation
 import UIKit
 
-public typealias GravatarDownloadProgressBlock = ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)
-
 /// Represents the result of a  Gravatar image download task.
 public struct GravatarImageDownloadResult {
     /// Gets the image of this result.
