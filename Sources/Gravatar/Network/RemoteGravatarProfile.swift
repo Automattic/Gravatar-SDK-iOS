43724af4f9de66aa48b12084157f5c867f32f20d
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-01-31 10:05:02 +0100

    Move `ProfileRemote` to its own file

--- Sources/Gravatar/Network/RemoteGravatarProfile.swift
+++ Sources/Gravatar/Network/RemoteGravatarProfile.swift
@@ -32,23 +32,3 @@ public class RemoteGravatarProfile {
         currentLocation = dictionary.string(forKey: "currentLocation") ?? ""
     }
 }
-
-struct FetchProfileResponse: Decodable {
-    let entry: [ProfileRemote]
-}
-
-struct ProfileRemote: Decodable {
-    let hash: String
-    let requestHash: String
-    let profileUrl: String
-    let preferredUsername: String
-    let thumbnailUrl: String
-    let displayName: String
-    let name: ProfileName?
-}
-
-public struct ProfileName: Decodable {
-    let givenName: String
-    let familyName: String
-    let formatted: String
-}
