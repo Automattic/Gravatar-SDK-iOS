0d323dac8d193714dbd208852fb92f8abd14656c
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-02-08 10:04:41 +0100

    Removing `taskIdentifier` from `CancellableDataTask` protocol

--- Sources/Gravatar/Network/CancellableDataTask.swift
+++ Sources/Gravatar/Network/CancellableDataTask.swift
@@ -3,13 +3,8 @@ import Foundation
 /// Allows you to cancel the data task.
 public protocol CancellableDataTask {
     func cancel()
-    var taskIdentifier: Int { get }
 }
 
 extension URLSessionTask: CancellableDataTask { }
 
-extension Task: CancellableDataTask {
-    public var taskIdentifier: Int {
-        hashValue
-    }
-}
+extension Task: CancellableDataTask { }
