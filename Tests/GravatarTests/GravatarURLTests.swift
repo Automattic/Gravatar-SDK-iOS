5474ceb178e6dfc77903bdf4f1ae6adf0fdd4612
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-02-20 11:47:17 -0600

    Add test for UrlWithForceImageDefaultFalse

--- Tests/GravatarTests/GravatarURLTests.swift
+++ Tests/GravatarTests/GravatarURLTests.swift
@@ -63,6 +63,14 @@ final class GravatarURLTests: XCTestCase {
         XCTAssertEqual(url?.url(with: options).query, nil)
         XCTAssertEqual(url?.url(with: options.updating(forceDefaultImage: true)).query, "f=y")
     }
+    
+    func testUrlWithForceImageDefaultFalse() {
+        let url = GravatarURL(verifiedGravatarURL)
+        XCTAssertNotNil(url)
+        let options = GravatarImageDownloadOptions()
+        XCTAssertEqual(url?.url(with: options).query, nil)
+        XCTAssertEqual(url?.url(with: options.updating(forceDefaultImage: false)).query, "f=n")
+    }
 
     func testCreateGravatarUrlWithEmail() throws {
         let options = GravatarImageDownloadOptions()
