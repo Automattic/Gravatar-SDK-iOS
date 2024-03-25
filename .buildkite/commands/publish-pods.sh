4986fa3019ae466d72df63019598aa581643a753
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-03-25 12:44:22 -0500

    Now that all podspecs import the same version, simplify the `publish-pods.sh` script

--- .buildkite/commands/publish-pods.sh
+++ .buildkite/commands/publish-pods.sh
@@ -1,35 +1,19 @@
 #!/bin/bash -eu
 
-if [ $# -eq 0 ]; then
-    echo "Error: CocoaPods publishing failed. All podspecs must be specified, separated by spaces."
+if [ $# -ne 1 ]; then
+    echo "Error: CocoaPods publishing failed. Specify a path to a podspec."
     exit 1
 fi
 
 SLACK_WEBHOOK=$PODS_SLACK_WEBHOOK
-PODSPEC_PATHS=("$@")
-
-# Verify that all versions are current before publishing any podspecs
-echo "--- :cocoapods: Verify version of all podspecs"
-invalid_pod=false
-for podspec_path in "${PODSPEC_PATHS[@]}"; do
-    pod_version=$(bundle exec pod ipc spec "$podspec_path" | jq -r '.version')
-    if [ -n "$BUILDKITE_TAG" ] && [ "$BUILDKITE_TAG" != "$pod_version" ]; then
-	    echo "Tag $BUILDKITE_TAG does not match version $pod_version from $podspec_path."
-	    invalid_pod=true
-    fi
-done
-
-if ( $invalid_pod ); then
-    exit 1
-fi
+PODSPEC_PATH="$1"
 
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
-for podspec_path in "${PODSPEC_PATHS[@]}"; do
-    echo "--- :cocoapods: Publishing Pod to CocoaPods CDN"
-    publish_pod $podspec_path
+echo "--- :cocoapods: Publishing Pod to CocoaPods CDN"
+publish_pod $PODSPEC_PATH
 
-    echo "--- :slack: Notifying Slack"
-    slack_notify_pod_published $podspec_path "$SLACK_WEBHOOK"
-done
+echo "--- :slack: Notifying Slack"
+slack_notify_pod_published $PODSPEC_PATH "$SLACK_WEBHOOK"
+
