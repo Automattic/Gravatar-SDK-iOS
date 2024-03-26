066348c26804a4399eedf97e81edc2d68a675979
Pinar Olguc <pinarolguc@gmail.com>
2024-03-26 19:34:06 +0300

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Gravatar"
+            name: "Gravatar",
+            swiftSettings: [
+              .enableExperimentalFeature("StrictConcurrency")
+            ]
         ),
         .testTarget(
             name: "GravatarTests",
@@ -35,7 +38,10 @@ let package = Package(
         ),
         .target(
             name: "GravatarUI",
-            dependencies: ["Gravatar"]
+            dependencies: ["Gravatar"],
+            swiftSettings: [
+              .enableExperimentalFeature("StrictConcurrency")
+            ]
         ),
         .testTarget(
             name: "GravatarUITests",
