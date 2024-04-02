ceef65590d6eb7ebb0f75f20e448d2b34b7785f1
Pinar Olguc <pinarolguc@gmail.com>
2024-04-02 17:53:53 +0300

    Add Bundle extension to the TestHelpers module

---
+++ Sources/TestHelpers/Bundle+ResourceBundle.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+#if !SWIFT_PACKAGE
+private class BundleFinder: NSObject {}
+#endif
+
+extension Bundle {
+    /// Returns the GravatarTests Bundle
+    /// If installed via CocoaPods, this will be GravatarTestsResources.bundle,
+    /// otherwise it will be the module bundle.
+    ///
+    public class var testHelpersBundle: Bundle {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
+        let defaultBundle = Bundle(for: BundleFinder.self)
+        // If installed with CocoaPods, resources will be in TestHelpersResources.bundle
+        if let bundleURL = defaultBundle.resourceURL,
+           let resourceBundle = Bundle(url: bundleURL.appendingPathComponent("TestHelpersResources.bundle"))
+        {
+            return resourceBundle
+        }
+        // Otherwise, the default bundle is used for resources
+        return defaultBundle
+        #endif
+    }
+}
--- Sources/TestHelpers/ImageHelper.swift
+++ Sources/TestHelpers/ImageHelper.swift
@@ -18,7 +18,7 @@ public class ImageHelper {
     }
 
     public static func dataFromImage(named: String, type: String) -> Data? {
-        guard let url = Bundle.module.url(forResource: named, withExtension: type) else {
+        guard let url = Bundle.testHelpersBundle.url(forResource: named, withExtension: type) else {
             return nil
         }
         var data: Data? = nil
@@ -29,7 +29,7 @@ public class ImageHelper {
     }
 
     public static func image(named: String, type: String) -> UIImage? {
-        guard let path = Bundle.module.path(forResource: named, ofType: type) else {
+        guard let path = Bundle.testHelpersBundle.path(forResource: named, ofType: type) else {
             return nil
         }
         return UIImage(contentsOfFile: path)
