2aed9b58069d60e6531c73d816e9b9191b5aa7ce
Pinar Olguc <pinarolguc@gmail.com>
2024-04-02 11:23:27 +0300

    Update pod validation

--- .buildkite/commands/validate-pods.sh
+++ .buildkite/commands/validate-pods.sh
@@ -7,6 +7,5 @@ echo "--- :microscope: Validate Podspec"
 # For some reason this fixes a failure in `lib lint`
 # https://github.com/Automattic/buildkite-ci/issues/7
 xcrun simctl list >> /dev/null
-bundle exec pod lib lint \
-    --include-podspecs="*.podspec" \
-    --verbose --fail-fast
+bundle exec pod lib lint Gravatar.podspec --verbose --fail-fast
+bundle exec pod lib lint --include-podspecs=Gravatar.podspec GravatarUI.podspec --verbose --fail-fast
