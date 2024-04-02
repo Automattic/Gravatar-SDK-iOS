47fd60d4797f0e6ad23be8f8eac945f0f1b04aaa
Olivier Halligon <olivier.halligon@automattic.com>
2024-04-02 16:42:59 +0200

    Test WIP CI-Toolkit fix to support co-dependent pods validation/publication

--- .buildkite/commands/publish-pod.sh
+++ .buildkite/commands/publish-pod.sh
@@ -11,8 +11,8 @@ PODSPEC_PATH="$1"
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
-echo "--- :cocoapods: Publishing Pod to CocoaPods CDN"
-publish_pod "$PODSPEC_PATH"
+echo "--- :cocoapods: Publishing $PODSPEC_PATH to CocoaPods CDN"
+publish_pod --synchronous "$PODSPEC_PATH"
 
 echo "--- :slack: Notifying Slack"
 slack_notify_pod_published "$PODSPEC_PATH" "$SLACK_WEBHOOK"
