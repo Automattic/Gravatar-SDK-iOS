3875eb6cf0d9992a515826f693ffad11680597a1
Pinar Olguc <pinarolguc@gmail.com>
2024-04-03 19:07:57 +0300

    Remove email

--- Demo/Demo/Gravatar-Demo/DemoProfileCardViewController.swift
+++ Demo/Demo/Gravatar-Demo/DemoProfileCardViewController.swift
@@ -12,7 +12,6 @@ class DemoProfileCardViewController: UIViewController {
         textField.autocapitalizationType = .none
         textField.textContentType = .emailAddress
         textField.textAlignment = .center
-        textField.text = "pinarolguc@gmail.com"
         return textField
     }()
     
