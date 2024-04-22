626e5d6fa22a62fc75b3f78df0d444b048da93a5
Pinar Olguc <pinar.olguc@automattic.com>
2024-04-22 15:38:51 +0300

    Small update

--- Sources/GravatarUI/DesignSystem/UIColor+DesignSystem.swift
+++ Sources/GravatarUI/DesignSystem/UIColor+DesignSystem.swift
@@ -17,10 +17,10 @@ extension UIColor {
     // Generated names are from https://colornamer.robertcooper.me/
     static let gravatarBlack: UIColor = .rgba(16, 21, 23)
     static let snowflakeWhite: UIColor = .rgba(240, 240, 240)
+    static let porpoiseGray: UIColor = .rgba(218, 218, 218)
     static let bleachedSilkWhite: UIColor = .rgba(242, 242, 242)
     static let smokeWhite: UIColor = .rgba(229, 231, 233)
     static let snowflakeWhite60: UIColor = snowflakeWhite.withAlphaComponent(0.6)
-    static let porpoiseGray: UIColor = .rgba(218, 218, 218)
     static let boatAnchorGray: UIColor = .rgba(107, 107, 107)
     static let spanishGray: UIColor = .rgba(151, 151, 151)
     static let dugongGray: UIColor = .rgba(112, 112, 112)
