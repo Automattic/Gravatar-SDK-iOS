31d55bbecf08681264fe2825b93bc492e304d5d4
Pinar Olguc <pinar.olguc@automattic.com>
2024-04-23 10:28:40 +0300

    Small update

--- Sources/GravatarUI/Base/UIView+Additions.swift
+++ Sources/GravatarUI/Base/UIView+Additions.swift
@@ -5,6 +5,7 @@ extension UIView {
         let spacer = UIView()
         spacer.translatesAutoresizingMaskIntoConstraints = false
         spacer.setContentHuggingPriority(.defaultLow, for: .vertical)
+        spacer.setContentHuggingPriority(.defaultLow, for: .horizontal)
         return spacer
     }
 }
