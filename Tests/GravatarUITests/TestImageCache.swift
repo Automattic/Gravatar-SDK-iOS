c0296903e939e9ab0586efdf9bea3c1696827348
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-04-26 17:16:55 +0200

    Update TestImageCache.swift

--- Tests/GravatarUITests/TestImageCache.swift
+++ Tests/GravatarUITests/TestImageCache.swift
@@ -2,23 +2,31 @@ import Gravatar
 import UIKit
 
 actor TestImageCache: ImageCaching {
-    var dict: [URL: UIImage] = [:]
+    var dict: [String: CacheEntryWrapper] = [:]
 
     var getImageCallCount = 0
-    var setTaskCallCount = 0
     var setImageCallsCount = 0
+    var setTaskCallCount = 0
 
-    func setImage(_ image: UIImage, for key: URL) async {
-        setImageCallsCount += 1
-        dict[key] = image
+    func setEntry(_ entry: Gravatar.CacheEntry, for key: String) async {
+        switch entry {
+        case .inProgress:
+            setTaskCallCount += 1
+        case .ready:
+            setImageCallsCount += 1
+        }
+        dict[key] = CacheEntryWrapper(entry)
     }
 
-    func setTask(_ task: Task<UIImage, Error>, for key: URL) async {
-        setTaskCallCount += 1
-    }
-
-    func getImage(for key: URL) async throws -> UIImage? {
+    func getEntry(with key: String) async -> Gravatar.CacheEntry? {
         getImageCallCount += 1
-        return dict[key]
+        return dict[key]?.cacheEntry
+    }
+}
+
+class CacheEntryWrapper {
+    let cacheEntry: CacheEntry
+    init(_ cacheEntry: CacheEntry) {
+        self.cacheEntry = cacheEntry
     }
 }
