eb4d54cac4d81bd098c1aeb012434835c311fbb9
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-04-29 18:18:56 +0200

    Update GravatarImageCacheTests.swift

--- Tests/GravatarTests/GravatarImageCacheTests.swift
+++ Tests/GravatarTests/GravatarImageCacheTests.swift
@@ -4,14 +4,14 @@ import XCTest
 final class GravatarImageCacheTests: XCTestCase {
     private let key = "ImageKey"
 
-    func testSetAndGet() async throws {
+    func testSetAndGet() {
         let cache = ImageCache()
         cache.setEntry(.ready(ImageHelper.testImage), for: key)
         let image = cache.getEntry(with: key)
         XCTAssertNotNil(image)
     }
 
-    func testRequestingMultipleTimes() async throws {
+    func testRequestingMultipleTimes() {
         let cache = ImageCache()
         let task = Task<UIImage, Error> {
             ImageHelper.testImage
