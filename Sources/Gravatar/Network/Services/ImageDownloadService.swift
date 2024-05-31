e1585cb2032ac31b237c94098443d6c27bfb7156
Pinar Olguc <pinarolguc@gmail.com>
2024-05-31 18:21:00 +0300

    Split the big method

--- Sources/Gravatar/Network/Services/ImageDownloadService.swift
+++ Sources/Gravatar/Network/Services/ImageDownloadService.swift
@@ -23,28 +23,38 @@ public struct ImageDownloadService: ImageDownloader, Sendable {
     public func fetchImage(with url: URL, forceRefresh: Bool = false, processingMethod: ImageProcessingMethod = .common()) async throws -> ImageDownloadResult {
         let request = URLRequest.imageRequest(url: url, forceRefresh: forceRefresh)
 
-        if !forceRefresh, let entry = await imageCache.getEntry(with: url.absoluteString) {
-            switch entry {
-            case .inProgress(let task):
-                let image = try await task.value
-                return ImageDownloadResult(image: image, sourceURL: url)
-            case .ready(let image):
-                return ImageDownloadResult(image: image, sourceURL: url)
-            }
+        if !forceRefresh, let image = try await cachedImage(for: url) {
+            return ImageDownloadResult(image: image, sourceURL: url)
         }
         let task = Task<UIImage, Error> {
             try await fetchAndProcessImage(request: request, processor: processingMethod.processor)
         }
-        await imageCache.setEntry(.inProgress(task), for: url.absoluteString)
+        let image = try await awaitAndCacheImage(from: task, cacheKey: url.absoluteString)
+        return ImageDownloadResult(image: image, sourceURL: url)
+    }
+
+    private func cachedImage(for url: URL) async throws -> UIImage? {
+        guard let entry = await imageCache.getEntry(with: url.absoluteString) else { return nil }
+        switch entry {
+        case .inProgress(let task):
+            let image = try await task.value
+            return image
+        case .ready(let image):
+            return image
+        }
+    }
+
+    private func awaitAndCacheImage(from task: Task<UIImage, Error>, cacheKey key: String) async throws -> UIImage {
+        await imageCache.setEntry(.inProgress(task), for: key)
         let image: UIImage
         do {
             image = try await task.value
         } catch {
-            await imageCache.setEntry(nil, for: url.absoluteString)
+            await imageCache.setEntry(nil, for: key)
             throw error
         }
-        await imageCache.setEntry(.ready(image), for: url.absoluteString)
-        return ImageDownloadResult(image: image, sourceURL: url)
+        await imageCache.setEntry(.ready(image), for: key)
+        return image
     }
 
     private func fetchAndProcessImage(request: URLRequest, processor: ImageProcessor) async throws -> UIImage {
