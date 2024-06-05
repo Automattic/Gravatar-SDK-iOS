329f20771b52f6d8031bc8120f53b3ea6809ecb7
Pinar Olguc <pinarolguc@gmail.com>
2024-06-05 15:47:01 +0300

    swiftformat

--- Sources/GravatarUI/ProfileView/Placeholder/PlaceholderDisplayers/BackgroundColorPlaceholderDisplayer.swift
+++ Sources/GravatarUI/ProfileView/Placeholder/PlaceholderDisplayers/BackgroundColorPlaceholderDisplayer.swift
@@ -47,15 +47,14 @@ class BackgroundColorPlaceholderDisplayer<T: UIView>: PlaceholderDisplaying {
 
 @MainActor
 class AvatarPlaceholderDisplayer<T: UIView>: BackgroundColorPlaceholderDisplayer<T> {
-    
     private var originalBorderWidth: CGFloat = 0
-    
+
     override func showPlaceholder() {
         super.showPlaceholder()
         originalBorderWidth = baseView.layer.borderWidth
         baseView.layer.borderWidth = 0
     }
-    
+
     override func hidePlaceholder() {
         super.hidePlaceholder()
         baseView.layer.borderWidth = originalBorderWidth
