6df797a30437d5c65e8f7ee0686773c61ba15378
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-07-02 14:14:17 -0500

    Specify resources folder for GravatarUI target

--- Package.swift
+++ Package.swift
@@ -43,6 +43,7 @@ let package = Package(
         .target(
             name: "GravatarUI",
             dependencies: ["Gravatar"],
+            resources: [.process("Resources")],
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency")
             ]
