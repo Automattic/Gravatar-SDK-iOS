a11340223eeb2b48822e1fb102115e5e959197cf
Pinar Olguc <pinarolguc@gmail.com>
2024-07-08 19:01:40 +0300

    Some improvements

--- Sources/GravatarUI/SwiftUI/AvatarView.swift
+++ Sources/GravatarUI/SwiftUI/AvatarView.swift
@@ -1,84 +1,81 @@
-import SwiftUI
 import Gravatar
+import SwiftUI
 
-struct AvatarView<LoadingView: View, ClipShape: Shape, Content: View>: View {
-    
-    // Default content type
-    struct DefaultContent: View {
-        let image: Image
-        @Binding var clipShape: ClipShape
-        @Binding var borderColor: Color?
-        @Binding var borderWidth: CGFloat?
-        
-        var body: some View {
-            image
-                .resizable()
-                .scaledToFit()
-                .clipShape(clipShape)
-                .overlay(
-                    clipShape
-                        .stroke(borderColor ?? Color.clear, lineWidth: borderWidth ?? 0)
-                )
-        }
-    }
-    
+@MainActor
+struct AvatarView<LoadingView: View, Content: View>: View {
     typealias LoadingViewBlock = () -> LoadingView
     typealias ImageContentBlock = (_ image: Image, _ isPlaceholder: Bool) -> Content
+    @ViewBuilder private let loadingView: LoadingViewBlock?
     @Binding private var forceRefresh: Bool
-    @ViewBuilder let loadingView: LoadingViewBlock?
-    
-    let avatarURL: AvatarURL?
-    let placeholder: Image?
-    let cache: ImageCaching
-    let urlSession: URLSession
+    @State private var isLoading: Bool = false
+    private var avatarURL: AvatarURL?
+    private let placeholder: Image?
+    private let cache: ImageCaching
+    private let urlSession: URLSession
+    private let transaction: Transaction
     private let imageContent: ImageContentBlock
     
     /// Use this initilizer to fully customize the content `Image`.
-    public init(avatarURL: AvatarURL?,
-                placeholder: Image?,
-                cache: ImageCaching = ImageCache.shared,
-                urlSession: URLSession = .shared,
-                forceRefresh: Binding<Bool> = .constant(false),
-                loadingView: LoadingViewBlock?,
-                imageContent: @escaping ImageContentBlock) where ClipShape == Circle {
+    init(
+        avatarURL: AvatarURL?,
+        placeholder: Image?,
+        cache: ImageCaching = ImageCache.shared,
+        urlSession: URLSession = .shared,
+        forceRefresh: Binding<Bool> = .constant(false),
+        loadingView: LoadingViewBlock?,
+        transaction: Transaction = Transaction(),
+        imageContent: @escaping ImageContentBlock
+    ) {
         self.avatarURL = avatarURL
         self.placeholder = placeholder
         self.cache = cache
         self.loadingView = loadingView
         self.urlSession = urlSession
-        self.imageContent = imageContent
         self._forceRefresh = forceRefresh
+        self.imageContent = imageContent
+        self.transaction = transaction
     }
     
     /// Use this initilizer to create the content `Image` with a given clipShape, borderColor and borderWidth.
-    public init(avatarURL: AvatarURL?,
-                placeholder: Image?,
-                cache: ImageCaching = ImageCache.shared,
-                urlSession: URLSession = .shared,
-                forceRefresh: Binding<Bool> = .constant(false),
-                loadingView: LoadingViewBlock?,
-                clipShape: Binding<ClipShape> = .constant(Circle()),
-                borderColor: Binding<Color?> = .constant(nil),
-                borderWidth: Binding<CGFloat?> = .constant(nil)) where Content == AnyView {
-        self.avatarURL = avatarURL
-        self.placeholder = placeholder
-        self.cache = cache
-        self.loadingView = loadingView
-        self.urlSession = urlSession
-        self._forceRefresh = forceRefresh
-        self.imageContent = { image, isPlaceholder in
-            AnyView(DefaultContent(image: image,
-                                   clipShape: clipShape,
-                                   borderColor: borderColor,
-                                   borderWidth: borderWidth))
+    init<ClipShape>(
+        avatarURL: AvatarURL?,
+        placeholder: Image?,
+        cache: ImageCaching = ImageCache.shared,
+        urlSession: URLSession = .shared,
+        forceRefresh: Binding<Bool> = .constant(false),
+        loadingView: LoadingViewBlock?,
+        transaction: Transaction = Transaction(),
+        clipShape: ClipShape,
+        borderColor: Color = .clear,
+        borderWidth: CGFloat = 0
+    ) where Content == DefaultAvatarContent<ClipShape> {
+        self.init(
+            avatarURL: avatarURL,
+            placeholder: placeholder,
+            cache: cache,
+            urlSession: urlSession,
+            forceRefresh: forceRefresh,
+            loadingView: loadingView,
+            transaction: transaction
+        ) { image, _ in
+            DefaultAvatarContent(
+                image: image,
+                clipShape: clipShape,
+                borderColor: borderColor,
+                borderWidth: borderWidth
+            )
         }
     }
     
     var body: some View {
-        CachedAsyncImage(url: avatarURL?.url,
-                         cache: cache,
-                         urlSession: urlSession,
-                         forceRefresh: $forceRefresh) { phase, isLoading in
+        CachedAsyncImage(
+            url: avatarURL?.url,
+            cache: cache,
+            urlSession: urlSession,
+            forceRefresh: $forceRefresh,
+            transaction: transaction,
+            isLoading: $isLoading
+        ) { phase in
             ZStack {
                 content(for: phase)
                 
@@ -109,17 +106,42 @@ struct AvatarView<LoadingView: View, ClipShape: Shape, Content: View>: View {
 }
 
 #Preview {
-    let avatarURL = AvatarURL(url: URL(string: "https://gravatar.com/avatar/680ba0b75f610ad8d939dbbd416dcced2d858656bd593ae81380ae8d2423b8cd")!, options: .init())!
+    guard let avatarURL = AvatarURL(with: .email("email@google.com"),
+                                    options: .init(preferredSize: .points(100))) else {
+        return Text("Invalid URL")
+    }
     return AvatarView(
         avatarURL: avatarURL,
-        placeholder: Image(systemName: "person"),
+        placeholder: Image(systemName: "person")
+            .renderingMode(.template)
+            .resizable(),
         loadingView: {
             ProgressView()
                 .progressViewStyle(CircularProgressViewStyle())
         },
-        clipShape: .constant(RoundedRectangle(cornerRadius: 20)),
-        borderColor: .constant(.accentColor),
-        borderWidth: .constant(2)
+        transaction: Transaction(animation: .easeInOut(duration: 1)),
+        clipShape: RoundedRectangle(cornerRadius: 20),
+        borderColor: .accentColor,
+        borderWidth: 2
     )
     .frame(width: 100, height: 100, alignment: .center)
+}
+
+// Default content type
+struct DefaultAvatarContent<ClipShape: Shape>: View {
+    let image: Image
+    var clipShape: ClipShape
+    var borderColor: Color
+    var borderWidth: CGFloat
+    
+    var body: some View {
+        image
+            .resizable()
+            .scaledToFit()
+            .clipShape(clipShape)
+            .overlay(
+                clipShape
+                    .stroke(borderColor, lineWidth: borderWidth)
+            )
+    }
 }
--- Sources/GravatarUI/SwiftUI/CachedAsyncImage.swift
+++ Sources/GravatarUI/SwiftUI/CachedAsyncImage.swift
@@ -1,24 +1,23 @@
-import SwiftUI
 import Foundation
 import Gravatar
+import SwiftUI
 
 @MainActor
 struct CachedAsyncImage<Content>: View, Sendable where Content: View {
-    
     @State private var phase: AsyncImagePhase
     @Binding private var forceRefresh: Bool
-    @State private var isLoading: Bool
+    @Binding private var isLoading: Bool
     
     private let url: URL?
     private let urlSession: URLSession
     private let scale: CGFloat
     private let transaction: Transaction
-    private let content: (AsyncImagePhase, Bool) -> Content
+    private let content: (AsyncImagePhase) -> Content
     private let cache: ImageCaching
     private let imageDownloader: ImageDownloader
     
-    public var body: some View {
-        content(phase, isLoading)
+    var body: some View {
+        content(phase)
             .onChange(of: forceRefresh) { newValue in
                 if newValue {
                     Task {
@@ -31,49 +30,43 @@ struct CachedAsyncImage<Content>: View, Sendable where Content: View {
             }
     }
     
-    public init(url: URL?,
-                cache: ImageCaching = ImageCache.shared,
-                urlSession: URLSession = .shared,
-                forceRefresh: Binding<Bool> = .constant(false),
-                scale: CGFloat = UITraitCollection.current.displayScale,
-                transaction: Transaction = Transaction(),
-                @ViewBuilder content: @escaping (AsyncImagePhase, Bool) -> Content) {
+    init(
+        url: URL?,
+        cache: ImageCaching = ImageCache.shared,
+        urlSession: URLSession = .shared,
+        forceRefresh: Binding<Bool> = .constant(false),
+        scale: CGFloat = UITraitCollection.current.displayScale,
+        transaction: Transaction = Transaction(),
+        isLoading: Binding<Bool> = .constant(false),
+        @ViewBuilder content: @escaping (AsyncImagePhase) -> Content
+    ) {
         self.url = url
         self.urlSession = urlSession
         self.scale = scale
         self.transaction = transaction
         self.content = content
         self._forceRefresh = forceRefresh
-        self._isLoading = State(wrappedValue: false)
+        self._isLoading = isLoading
         self._phase = State(wrappedValue: .empty)
         self.cache = cache
         self.imageDownloader = ImageDownloadService(urlSession: urlSession, cache: cache)
-        print("CachedAsyncImage init")
     }
     
     @Sendable
     private func load() async {
-        print("start - \(String(describing: phase))")
-        defer { print("end - \(String(describing: phase))") }
         guard let url else {
             withAnimation(transaction.animation) {
                 phase = .empty
             }
             return
         }
-        
-        if !forceRefresh, let cacheEntry = await cache.getEntry(with: url.absoluteString) {
-            switch cacheEntry {
-            case .ready(let uiImage):
-                phase = .success(Image(uiImage: uiImage))
-                return
-            case .inProgress:
-                break
-            }
-        }
         do {
             isLoading = true
-            let result = try await imageDownloader.fetchImage(with: url, forceRefresh: forceRefresh, processingMethod: .common(scaleFactor: scale))
+            let result = try await imageDownloader.fetchImage(
+                with: url,
+                forceRefresh: forceRefresh,
+                processingMethod: .common(scaleFactor: scale)
+            )
             withAnimation(transaction.animation) {
                 phase = .success(Image(uiImage: result.image))
             }
@@ -88,7 +81,14 @@ struct CachedAsyncImage<Content>: View, Sendable where Content: View {
 }
 
 #Preview {
-    CachedAsyncImage(url: URL(string: "https://gravatar.com/avatar/680ba0b75f610ad8d939dbbd416dcced2d858656bd593ae81380ae8d2423b8cd?s=300")) { phase, isLoading in
+    guard let avatarURL = AvatarURL(with: .email("email@google.com"),
+                                    options: .init(preferredSize: .points(100))) else {
+        return Text("Invalid URL")
+    }
+    return CachedAsyncImage(
+        url: avatarURL.url,
+        transaction: Transaction(animation: .easeInOut(duration: 1))
+    ) { phase in
         switch phase {
         case .empty:
             Text("empty")
