458a6b7a410504944a5b409cb50b6e576784c230
etoledom <eduardo.toledo@automattic.com>
2024-07-11 09:16:39 +0200

    Deprecating public CodingKeys and init methods (#316)

--- Sources/Gravatar/OpenApi/Generated/CryptoWalletAddress.swift
+++ Sources/Gravatar/OpenApi/Generated/CryptoWalletAddress.swift
@@ -8,12 +8,19 @@ public struct CryptoWalletAddress: Codable, Hashable, Sendable {
     /// The wallet address for the crypto currency.
     public private(set) var address: String
 
+    @available(*, deprecated, message: "init will become internal on the next release")
     public init(label: String, address: String) {
         self.label = label
         self.address = address
     }
 
+    @available(*, deprecated, message: "CodingKeys will become internal on the next release.")
     public enum CodingKeys: String, CodingKey, CaseIterable {
+        case label
+        case address
+    }
+
+    enum InternalCodingKeys: String, CodingKey, CaseIterable {
         case label
         case address
     }
@@ -21,7 +28,7 @@ public struct CryptoWalletAddress: Codable, Hashable, Sendable {
     // Encodable protocol methods
 
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
+        var container = encoder.container(keyedBy: InternalCodingKeys.self)
         try container.encode(label, forKey: .label)
         try container.encode(address, forKey: .address)
     }
