a9e3ca0bfd63ae0d113edfcfd6411f9395cbb83c
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-07-24 16:34:16 -0500

    Rename to `UIKit-Demo`

--- .buildkite/commands/build-demos.sh
+++ .buildkite/commands/build-demos.sh
@@ -4,7 +4,7 @@ echo "--- :rubygems: Setting up Gems"
 install_gems
 
 echo "--- 🛠 Building Demo (Swift)"
-bundle exec fastlane build_demo scheme:Swift-Demo
+bundle exec fastlane build_demo scheme:UIKit-Demo
 
 echo "--- 🛠 Building Demo (SwiftUI)"
 bundle exec fastlane build_demo scheme:SwiftUI-Demo
