aaa7346714e57e8b00a22585e569e4d3276ab75a
Gio Lodi <gio@mokacoding.com>
2024-07-25 10:11:36 +1000

    Reindent `fastlane/lib/env_manager.rb` using 2 spaces

--- fastlane/lib/env_manager.rb
+++ fastlane/lib/env_manager.rb
@@ -7,65 +7,65 @@ require 'fastlane'
 
 # Manages loading of environment variables from a .env and accessing them in a user-friendly way.
 class EnvManager
-    @env_path = nil
-    @env_example_path = nil
-    @print_error_method = nil
+  @env_path = nil
+  @env_example_path = nil
+  @print_error_method = nil
 
-    # Set up by loading the .env file with the given name.
-    #
-    # TODO: We could go one step and guess the name based on the repo URL.
-    def self.set_up(
-        env_file_name:,
-        env_file_folder: File.join(Dir.home, '.a8c-apps'),
-        example_env_file_path: 'fastlane/example.env',
-        print_error_method: ->(message) { FastlaneCore::UI.user_error!(message) }
-    )
-        @env_path = File.join(env_file_folder, env_file_name)
-        @env_example_path = example_env_file_path
-        @print_error_method = print_error_method
+  # Set up by loading the .env file with the given name.
+  #
+  # TODO: We could go one step and guess the name based on the repo URL.
+  def self.set_up(
+    env_file_name:,
+    env_file_folder: File.join(Dir.home, '.a8c-apps'),
+    example_env_file_path: 'fastlane/example.env',
+    print_error_method: ->(message) { FastlaneCore::UI.user_error!(message) }
+  )
+    @env_path = File.join(env_file_folder, env_file_name)
+    @env_example_path = example_env_file_path
+    @print_error_method = print_error_method
 
-        # We don't check for @env_path to exist here
-        Dotenv.load(@env_path)
+    # We don't check for @env_path to exist here
+    Dotenv.load(@env_path)
+  end
+
+  # Use this instead of getting values from `ENV` directly. It will throw an error if the requested value is missing or empty.
+  def self.get_required_env!(key)
+    unless ENV.key?(key)
+      message = "Environment variable '#{key}' is not set."
+
+      if running_on_ci?
+        @print_error_method.call(message)
+      elsif File.exist?(@env_path)
+        @print_error_method.call("#{message} Consider adding it to #{@env_path}.")
+      else
+        env_file_dir = File.dirname(@env_path)
+        env_file_name = File.basename(@env_path)
+
+        @print_error_method.call <<~MSG
+          #{env_file_name} not found in #{env_file_dir} while looking for env var #{key}.
+
+          Please copy #{@env_example_path} to #{@env_path} and fill in the value for #{key}.
+
+          mkdir -p #{env_file_dir} && cp #{@env_example_path} #{@env_path}
+        MSG
+      end
     end
 
-    # Use this instead of getting values from `ENV` directly. It will throw an error if the requested value is missing or empty.
-    def self.get_required_env!(key)
-        unless ENV.key?(key)
-            message = "Environment variable '#{key}' is not set."
+    value = ENV.fetch(key)
 
-            if running_on_ci?
-                @print_error_method.call(message)
-            elsif File.exist?(@env_path)
-                @print_error_method.call("#{message} Consider adding it to #{@env_path}.")
-            else
-                env_file_dir = File.dirname(@env_path)
-                env_file_name = File.basename(@env_path)
+    UI.user_error!("Env var for key #{key} is set but empty. Please set a value for #{key}.") if value.to_s.empty?
 
-                @print_error_method.call <<~MSG
-                    #{env_file_name} not found in #{env_file_dir} while looking for env var #{key}.
+    value
+  end
 
-                    Please copy #{@env_example_path} to #{@env_path} and fill in the value for #{key}.
+  # Use this to ensure all env vars a lane requires are set.
+  #
+  # The best place to call this is at the start of a lane, to fail early.
+  def self.require_env_vars!(*keys)
+    keys.each { |key| get_required_env!(key) }
+  end
 
-                    mkdir -p #{env_file_dir} && cp #{@env_example_path} #{@env_path}
-                MSG
-            end
-        end
-
-        value = ENV.fetch(key)
-
-        UI.user_error!("Env var for key #{key} is set but empty. Please set a value for #{key}.") if value.to_s.empty?
-
-        value
-    end
-
-    # Use this to ensure all env vars a lane requires are set.
-    #
-    # The best place to call this is at the start of a lane, to fail early.
-    def self.require_env_vars!(*keys)
-        keys.each { |key| get_required_env!(key) }
-    end
-
-    def self.running_on_ci?
-        ENV['CI'] == 'true'
-    end
+  def self.running_on_ci?
+    ENV['CI'] == 'true'
+  end
 end
