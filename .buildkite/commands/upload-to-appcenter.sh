497e2fc9e6f4b70e2de6782b5df1f8012cfaccb7
Gio Lodi <gio@mokacoding.com>
2024-07-26 22:41:34 +1000

    Use artifact from current build when uploading to App Center

--- .buildkite/commands/upload-to-appcenter.sh
+++ .buildkite/commands/upload-to-appcenter.sh
@@ -4,9 +4,8 @@ NAME=$1
 NAME_LOWERCASED="$(echo "$NAME" | tr '[:upper:]' '[:lower:]')"
 
 echo "--- :arrow_down: Downloading Prototype Build"
-# TODO: remove build once verified
-buildkite-agent artifact download ".build/artifacts/*.ipa" . --step "build_$NAME_LOWERCASED" --build "0190ee15-44cf-41ad-8132-7698c8a6db37"
-buildkite-agent artifact download ".build/artifacts/*.app.dSYM.zip" . --step "build_$NAME_LOWERCASED" --build "0190ee15-44cf-41ad-8132-7698c8a6db37"
+buildkite-agent artifact download ".build/artifacts/*.ipa" . --step "build_$NAME_LOWERCASED"
+buildkite-agent artifact download ".build/artifacts/*.app.dSYM.zip" . --step "build_$NAME_LOWERCASED"
 
 echo "--- :rubygems: Setting up Gems"
 install_gems
