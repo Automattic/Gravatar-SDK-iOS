01e50fc67ce405d6c771863a9b8a3a9fba812c19
Gio Lodi <gio@mokacoding.com>
2024-07-26 22:27:58 +1000

    Update build id in test script

--- .buildkite/commands/upload-to-appcenter.sh
+++ .buildkite/commands/upload-to-appcenter.sh
@@ -5,8 +5,8 @@ NAME_LOWERCASED="$(echo "$NAME" | tr '[:upper:]' '[:lower:]')"
 
 echo "--- :arrow_down: Downloading Prototype Build"
 # TODO: remove build once verified
-buildkite-agent artifact download ".build/artifacts/*.ipa" . --step "build_$NAME_LOWERCASED" --build "0190ee1c-6984-4fed-a59a-02f561c5ed2d"
-buildkite-agent artifact download ".build/artifacts/*.app.dSYM.zip" . --step "build_$NAME_LOWERCASED" --build "0190ee1c-6984-4fed-a59a-02f561c5ed2d"
+buildkite-agent artifact download ".build/artifacts/*.ipa" . --step "build_$NAME_LOWERCASED" --build "0190ee15-44cf-41ad-8132-7698c8a6db37"
+buildkite-agent artifact download ".build/artifacts/*.app.dSYM.zip" . --step "build_$NAME_LOWERCASED" --build "0190ee15-44cf-41ad-8132-7698c8a6db37"
 
 echo "--- :rubygems: Setting up Gems"
 install_gems
