26388a781446bd87574d345e0bb40ac0582a1733
Gio Lodi <gio@mokacoding.com>
2024-07-26 22:22:26 +1000

    Remove weird `iecho` typo

--- .buildkite/commands/upload-to-appcenter.sh
+++ .buildkite/commands/upload-to-appcenter.sh
@@ -3,7 +3,7 @@
 NAME=$1
 NAME_LOWERCASED="$(echo "$NAME" | tr '[:upper:]' '[:lower:]')"
 
-iecho "--- :arrow_down: Downloading Prototype Build"
+echo "--- :arrow_down: Downloading Prototype Build"
 # TODO: remove build once verified
 buildkite-agent artifact download ".build/artifacts/*.ipa" . --step "build_$NAME_LOWERCASED" --build "0190ee1c-6984-4fed-a59a-02f561c5ed2d"
 buildkite-agent artifact download ".build/artifacts/*.app.dSYM.zip" . --step "build_$NAME_LOWERCASED" --build "0190ee1c-6984-4fed-a59a-02f561c5ed2d"
