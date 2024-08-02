0c915021d24f9e6eb1c848fd5ff11acb6d0d80f9
Pinar Olguc <pinar.olguc@automattic.com>
2024-08-02 17:28:54 +0300

    Put a profile card into the avatar picker (#331)

---
+++ Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerProfileView.swift
@@ -0,0 +1,112 @@
+import Gravatar
+import SwiftUI
+
+@MainActor
+struct AvatarPickerProfileView: View {
+    private enum Constants {
+        static let avatarLength: CGFloat = 72
+    }
+
+    struct Model {
+        var displayName: String
+        var location: String
+        var profileURL: URL?
+    }
+
+    @Binding var avatarIdentifier: AvatarIdentifier?
+    @Binding var model: Model?
+    @Binding var isLoading: Bool
+    @StateObject private var placeholderColorManager: ProfileViewPlaceholderColorManager = .init()
+
+    private(set) var viewProfileAction: ((URL?) -> Void)? = nil
+
+    private var avatarURL: URL? {
+        guard let avatarIdentifier else { return nil }
+        return AvatarURL(
+            with: avatarIdentifier,
+            options: .init(
+                preferredSize: .points(Constants.avatarLength),
+                defaultAvatarOption: .status404
+            )
+        )?.url
+    }
+
+    var body: some View {
+        HStack(alignment: .center, spacing: .DS.Padding.single) {
+            avatarView()
+            if let model {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(model.displayName)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                    Text(model.location)
+                        .font(.footnote)
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                    Button("Discover Your Gravatar Card →") {
+                        viewProfileAction?(model.profileURL)
+                    }
+                    .font(.footnote)
+                    .foregroundColor(Color(UIColor.label))
+                }
+            } else {
+                emptyViews()
+            }
+        }
+        .onChange(of: isLoading) { newValue in
+            placeholderColorManager.toggleAnimation(newValue)
+        }
+        .onAppear {
+            placeholderColorManager.toggleAnimation(isLoading)
+        }
+    }
+
+    func emptyViews() -> some View {
+        VStack(alignment: .leading, spacing: .DS.Padding.half, content: {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: 180, height: 24)
+            RoundedRectangle(cornerRadius: 6)
+                .frame(width: 100, height: 12)
+            RoundedRectangle(cornerRadius: 6)
+                .frame(width: 140, height: 12)
+        })
+        .foregroundColor(placeholderColorManager.placeholderColor)
+    }
+
+    func avatarView() -> some View {
+        AvatarView(
+            url: avatarURL,
+            placeholder: nil,
+            loadingView: {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+            }
+        )
+        .scaledToFill()
+        .frame(width: Constants.avatarLength, height: Constants.avatarLength)
+        .background(placeholderColorManager.placeholderColor)
+        .aspectRatio(1, contentMode: .fill)
+        .shape(Circle())
+    }
+}
+
+#Preview {
+    AvatarPickerProfileView(
+        avatarIdentifier: .constant(nil),
+        model: .constant(
+            .init(
+                displayName: "Shelly Kimbrough",
+                location: "San Antonio, TX",
+                profileURL: URL(string: "https://gravatar.com")
+            )
+        ),
+        isLoading: .constant(false)
+    )
+}
+
+#Preview("Empty") {
+    AvatarPickerProfileView(avatarIdentifier: .constant(nil), model: .constant(nil), isLoading: .constant(false))
+}
+
+#Preview("Empty & Loading") {
+    AvatarPickerProfileView(avatarIdentifier: .constant(nil), model: .constant(nil), isLoading: .constant(true))
+}
--- Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerViewModel.swift
+++ Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerViewModel.swift
@@ -1,28 +1,56 @@
 import Foundation
+import Gravatar
 import SwiftUI
 
 @MainActor
 class AvatarPickerViewModel: ObservableObject {
     private let profileService: ProfileService = .init()
-    private var email: Email?
+    private var email: Email? {
+        didSet {
+            guard let email else {
+                avatarIdentifier = nil
+                return
+            }
+            avatarIdentifier = .email(email)
+        }
+    }
+
     private var authToken: String?
     @Published private(set) var avatarsResult: Result<[AvatarImageModel], Error>?
     @Published private(set) var currentAvatarResult: Result<String, Error>?
+    private var profileResult: Result<ProfileSummaryModel, Error>? {
+        didSet {
+            switch profileResult {
+            case .success(let value):
+                profileModel = .init(displayName: value.displayName, location: value.location, profileURL: value.profileURL)
+            default:
+                profileModel = nil
+            }
+        }
+    }
+
+    @Published var isProfileLoading: Bool = false
     @Published private(set) var isAvatarsLoading: Bool = false
+    @Published var avatarIdentifier: AvatarIdentifier?
+    @Published var profileModel: AvatarPickerProfileView.Model?
 
     init(email: Email, authToken: String) {
         self.email = email
+        avatarIdentifier = .email(email)
         self.authToken = authToken
     }
 
     /// Internal init for previewing purposes. Do not make this public.
-    init(avatarImageModels: [AvatarImageModel], selectedImageID: String? = nil) {
+    init(avatarImageModels: [AvatarImageModel], selectedImageID: String? = nil, profileModel: ProfileSummaryModel? = nil) {
         if let selectedImageID {
             self.currentAvatarResult = .success(selectedImageID)
         } else {
             self.currentAvatarResult = nil
         }
         self.avatarsResult = .success(avatarImageModels)
+        if let profileModel {
+            self.profileResult = .success(profileModel)
+        }
     }
 
     func fetchAvatars() async {
@@ -42,6 +70,19 @@ class AvatarPickerViewModel: ObservableObject {
         }
     }
 
+    func fetchProfile() async {
+        guard let email else { return }
+        do {
+            isProfileLoading = true
+            let profile = try await profileService.fetch(with: .email(email))
+            profileResult = .success(profile)
+            isProfileLoading = false
+        } catch {
+            profileResult = .failure(error)
+            isProfileLoading = false
+        }
+    }
+
     func fetchIdentity() async {
         guard let authToken, let email else { return }
         do {
@@ -55,7 +96,12 @@ class AvatarPickerViewModel: ObservableObject {
     func update(email: String) {
         self.email = .init(email)
         Task {
-            await fetchIdentity()
+            // parallel child tasks
+            async let identity: () = fetchIdentity()
+            async let profile: () = fetchProfile()
+
+            await identity
+            await profile
         }
     }
 
@@ -66,8 +112,15 @@ class AvatarPickerViewModel: ObservableObject {
 
     func refresh() {
         Task {
-            await fetchAvatars()
-            await fetchIdentity()
+            // We want them to be parallel child tasks so they don't wait each other.
+            async let avatars: () = fetchAvatars()
+            async let identity: () = fetchIdentity()
+            async let profile: () = fetchProfile()
+
+            // We need to await them otherwise network requests can be cancelled.
+            await avatars
+            await identity
+            await profile
         }
     }
 }
