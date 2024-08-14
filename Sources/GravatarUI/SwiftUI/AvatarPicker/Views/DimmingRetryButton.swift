59e44eb9cfb20bbc2e711a92c812db794797e5b4
Pinar Olguc <pinarolguc@gmail.com>
2024-08-14 18:30:43 +0300

    Rename

--- Sources/GravatarUI/SwiftUI/AvatarPicker/Views/DimmingRetryButton.swift
+++ Sources/GravatarUI/SwiftUI/AvatarPicker/Views/DimmingRetryButton.swift
@@ -2,21 +2,22 @@ import SwiftUI
 
 /// Dims the parent and puts a retry button on it.
 struct DimmingRetryButton: View {
-    let onTap: () -> Void
+    let action: () -> Void
 
     var body: some View {
-        Button(action: {
-            onTap()
-        }, label: {
-            ZStack {
-                Rectangle()
-                    .fill(.black.opacity(0.3))
-                Image(systemName: "arrow.clockwise")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 20, height: 20)
+        Button(
+            action: action,
+            label: {
+                ZStack {
+                    Rectangle()
+                        .fill(.black.opacity(0.3))
+                    Image(systemName: "arrow.clockwise")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 20, height: 20)
+                }
             }
-        })
+        )
         .frame(width: .infinity, height: .infinity)
         .foregroundColor(Color.white)
     }
