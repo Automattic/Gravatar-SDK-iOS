4a4dd0b8d4fefcdd2b31810b8c5da850627895ce
Pinar Olguc <pinarolguc@gmail.com>
2024-08-22 18:42:01 +0300

    Replace sheet with fullScreenCover for Safari

--- Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerView.swift
+++ Sources/GravatarUI/SwiftUI/AvatarPicker/AvatarPickerView.swift
@@ -65,7 +65,7 @@ struct AvatarPickerView: View {
                 }
             }
         }
-        .sheet(item: $safariURL) { url in
+        .fullScreenCover(item: $safariURL) { url in
             SafariView(url: url)
                 .edgesIgnoringSafeArea(.all)
         }
