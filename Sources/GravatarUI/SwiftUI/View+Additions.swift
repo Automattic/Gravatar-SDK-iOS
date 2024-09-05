8ab6a6d64aa96c510d22568ab2188d68f6b173f2
Pinar Olguc <pinarolguc@gmail.com>
2024-09-05 14:31:31 +0300

    Remove unused

--- Sources/GravatarUI/SwiftUI/View+Additions.swift
+++ Sources/GravatarUI/SwiftUI/View+Additions.swift
@@ -108,15 +108,6 @@ extension View {
         }
     }
 
-    @ViewBuilder
-    func `if`(_ condition: Bool, apply: (Self) -> some View) -> some View {
-        if condition {
-            apply(self)
-        } else {
-            self
-        }
-    }
-
     /// Caution: `InnerHeightPreferenceKey` accumulates the values so DO NOT use this on  a View and one of its ancestors at the same time.
     @ViewBuilder
     func accumulateIntrinsicHeight() -> some View {
