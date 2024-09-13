f3359a3218c4dd56238bb45adbe8d76e0072d3fd
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-09-13 13:44:48 -0500

    Encode URLQueryItem values using a custom CharacterSet

--- Sources/Gravatar/URLComponents+Additions.swift
+++ Sources/Gravatar/URLComponents+Additions.swift
@@ -1,35 +1,59 @@
 import Foundation
 
 extension URLComponents {
-    /// Returns a `URLComponents` with the `queryItems` set.
-    ///
-    /// The default behavior is to percent-encode all `+` characters.  This differs from
-    /// the default behavior of `URLComponents`.  By default, `URLComponents` doesn't encode
-    /// the `+` character because it is a valid character in a query. This can cause problems when
-    /// query values contain a literal `+` character, which won't be interpreted as a `+`.
-    ///
-    /// Note that this does not affect the `.queryItems` of the object.  It only makes changes to the
-    /// `.percentEncodedQuery`.
+    /// Returns a `URLComponents` with the `queryItems` set and optinally encoded.
     ///
     /// - Parameters:
     ///   - queryItems: An array of `URLQueryItem`s
-    ///   - plusSignLiteralEncoded: Whether to fully percent-encode values.
-    ///         Setting to `true` will encode the `+` character.
-    ///         Setting to `false` will leave the default encoding in place
+    ///   - urlEncodedValues: Whether to use url-encoded values.
+    ///         Setting to `true` will use url-encoding for values.
+    ///         Setting to `false` will encode only characters that are not valid in a url query
     /// - Returns: `URLComponents` with the `queryItems` set with the specified encoding
-    package func withQueryItems(_ queryItems: [URLQueryItem], plusSignLiteralEncoded: Bool = true) -> URLComponents {
+    package func withQueryItems(_ queryItems: [URLQueryItem], urlEncodedValues: Bool = true) -> URLComponents {
         var copy = self
 
-        guard queryItems.isEmpty == false else {
-            return copy
-        }
-
-        copy.queryItems = queryItems
-
-        if plusSignLiteralEncoded {
-            copy.percentEncodedQuery = copy.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+        if queryItems.isEmpty {
+            copy.queryItems = nil
+        } else if urlEncodedValues {
+            copy.percentEncodedQueryItems = queryItems.map { $0.percentEncodedValue(withAllowedCharacters: .restAPI) }
+        } else {
+            copy.queryItems = queryItems
         }
 
         return copy
+    }
+}
+
+extension CharacterSet {
+    /// Defines a character set for URL Encoding based on `RFC 3986`
+    /// https://datatracker.ietf.org/doc/html/rfc3986/#page-12
+    ///
+    /// ## Unreserved Characters
+    /// - Uppercase and lowercase letters
+    /// - Decimal digits
+    /// - `- . _ ~`
+    ///
+    /// ## Reserved Characters
+    /// `: / ? # [ ] @ ! $ & ' ( ) * + , ; =`
+    fileprivate static var restAPI: CharacterSet {
+        .alphanumerics.union(CharacterSet(charactersIn: "-_.~"))
+    }
+}
+
+extension URLQueryItem {
+    /// Returns a `URLQueryItem` whose value has been percent-encoded for the specified character set.
+    ///
+    /// A Percent-encoded `URLQueryItem` should only be assigned to `URLComponents.percentEncodedQueryItems`.
+    /// It is a mistake to assign an encoded `URLQueryItem` to `URLComponents.queryItems`.
+    /// If a percent-encoded `URLQueryItem` is assigned as a `.queryItem`, the encoded value will be double-encoded.
+    ///
+    /// - Parameter withAllowedCharacters: The character set that should not be percent-encoded
+    /// - Returns: A `URLQueryItem` configured with the specified encoding
+    fileprivate func percentEncodedValue(withAllowedCharacters: CharacterSet) -> URLQueryItem {
+        var newQueryItem = self
+        newQueryItem.value = value?
+            .addingPercentEncoding(withAllowedCharacters: withAllowedCharacters)
+
+        return newQueryItem
     }
 }
--- Tests/GravatarTests/URLComponentsTests.swift
+++ Tests/GravatarTests/URLComponentsTests.swift
@@ -4,41 +4,70 @@ final class URLComponentsTests: XCTestCase {
     private let queryItems = [
         URLQueryItem(name: "spaces", value: "value with spaces"),
         URLQueryItem(name: "plus_signs", value: "value+with+plus+signs"),
-        URLQueryItem(name: "special_chars", value: "!#$&'()*+,/:;=?@[]"),
+        URLQueryItem(
+            name: "non_reserved_chars",
+            value: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~"
+        ),
+        URLQueryItem(name: "reserved_chars", value: "!*'();:@&=+$,/?%#[]"),
     ]
 
     private static let urlString = "https://example.com"
 
-    private enum PlusSignLiteralEncodedQuery {
+    private enum URLEncodedQuery {
         static let spaces_query = "spaces=value%20with%20spaces"
         static let plus_sign_query = "plus_signs=value%2Bwith%2Bplus%2Bsigns"
-        static let special_chars = "special_chars=!%23$%26'()*%2B,/:;%3D?@%5B%5D"
+        static let non_reserved_chars = "non_reserved_chars=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~"
+        static let reserved_chars = "reserved_chars=%21%2A%27%28%29%3B%3A%40%26%3D%2B%24%2C%2F%3F%25%23%5B%5D"
 
-        static var queryString: String { "\(spaces_query)&\(plus_sign_query)&\(special_chars)" }
+        static var queryString: String {
+            "\(spaces_query)&\(plus_sign_query)&\(non_reserved_chars)&\(reserved_chars)"
+        }
+
         static var url: URL { URL(string: "\(urlString)?\(queryString)")! }
     }
 
     private enum DefaultEncodedQuery {
         static let spaces_query = "spaces=value%20with%20spaces"
         static let plus_sign_query = "plus_signs=value+with+plus+signs"
-        static let special_chars = "special_chars=!%23$%26'()*+,/:;%3D?@%5B%5D"
+        static let non_reserved_chars = "non_reserved_chars=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~"
+        static let reserved_chars = "reserved_chars=!*'();:@%26%3D+$,/?%25%23%5B%5D"
 
-        static var queryString: String { "\(spaces_query)&\(plus_sign_query)&\(special_chars)" }
+        static var queryString: String {
+            "\(spaces_query)&\(plus_sign_query)&\(non_reserved_chars)&\(reserved_chars)"
+        }
+
         static var url: URL { URL(string: "\(urlString)?\(queryString)")! }
     }
 
-    func testSetQueryItemsWithUrlEncoding() {
-        let components = URLComponents(string: Self.urlString)
-        let sut = components?.withQueryItems(queryItems, plusSignLiteralEncoded: true)
+    func testURLComponentsEncodingBehaviorMatchesDefaultBehavior() {
+        var reference = URLComponents(string: Self.urlString)
+        reference?.queryItems = queryItems
 
-        let reference = PlusSignLiteralEncodedQuery.url
+        let sut = URLComponents(string: Self.urlString, queryItems: queryItems, urlEncodedValues: false)
+
+        XCTAssertEqual(sut?.url, reference?.url)
+    }
+
+    func testURLComponentsEncodingBehaviorEncodesSpecialCharacters() {
+        let reference = URLEncodedQuery.url
+
+        let sut = URLComponents(string: Self.urlString, queryItems: queryItems, urlEncodedValues: true)
 
         XCTAssertEqual(sut?.url, reference)
     }
 
-    func testSetQueryItemsWithoutUrlEncoding() {
+    func testSetQueryItemsWithPercentEncoding() {
         let components = URLComponents(string: Self.urlString)
-        let sut = components?.withQueryItems(queryItems, plusSignLiteralEncoded: false)
+        let sut = components?.withQueryItems(queryItems, urlEncodedValues: true)
+
+        let reference = URLEncodedQuery.url
+
+        XCTAssertEqual(sut?.url, reference)
+    }
+
+    func testSetQueryItemsWithoutPercentEncoding() {
+        let components = URLComponents(string: Self.urlString)
+        let sut = components?.withQueryItems(queryItems, urlEncodedValues: false)
 
         let reference = DefaultEncodedQuery.url
 
@@ -47,7 +76,7 @@ final class URLComponentsTests: XCTestCase {
 
     func testSetQueryItemsWithEmptyArray() {
         let components = URLComponents(string: Self.urlString)
-        let sut = components?.withQueryItems([], plusSignLiteralEncoded: false)
+        let sut = components?.withQueryItems([], urlEncodedValues: false)
 
         XCTAssertNotNil(sut)
         XCTAssertNil(sut?.queryItems)
