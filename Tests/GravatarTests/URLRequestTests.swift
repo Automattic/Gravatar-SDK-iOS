c08aeb2e9f81c05a678ab889e7445794612b6fea
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-09-19 15:48:58 -0500

    Refactor to use `settingHeader()`

--- Tests/GravatarTests/URLRequestTests.swift
+++ Tests/GravatarTests/URLRequestTests.swift
@@ -4,29 +4,27 @@ import XCTest
 final class URLRequestTests: XCTestCase {
     private let url = URL(string: "https://fake.url")!
     private let acceptLanguageHeaderName = "Accept-Language"
+    private let mockLanguagePreferencePovider = MockLanguagePreferenceProvider(
+        maxPreferredLanguages: 6,
+        preferredLanguages: [ // One more than `maxPreferredLanguages`
+            "en",
+            "fr",
+            "jp",
+            "quenyan",
+            "sindarin",
+            "dwarven",
+            "mordor",
+        ]
+    )
 
     func testSettingAcceptLanguageToFrench() throws {
         let encoding = "fr"
-
         let urlRequest = URLRequest(url: url).settingAcceptLanguage(encoding)
 
         try urlRequest.expect(header: acceptLanguageHeaderName, withValue: encoding)
     }
 
     func testSettingAcceptLanguageUsingPreferredLanguages() throws {
-        let mockLanguagePreferencePovider = MockLanguagePreferenceProvider(
-            maxPreferredLanguages: 6,
-            preferredLanguages: [ // One more than `maxPreferredLanguages`
-                "en",
-                "fr",
-                "jp",
-                "quenyan",
-                "sindarin",
-                "dwarven",
-                "mordor",
-            ]
-        )
-
         let urlRequest = URLRequest(url: url).settingDefaultAcceptLanguage(languagePreferenceProvider: mockLanguagePreferencePovider)
 
         try urlRequest.expect(
@@ -37,10 +35,8 @@ final class URLRequestTests: XCTestCase {
 
     func testQualityEncoding() {
         let preferredLanguages = ["en", "fr", "jp", "quenyan", "sindarin", "dwarven"]
-
-        let encodedReference = "en, fr;q=0.9, jp;q=0.8, quenyan;q=0.7, sindarin;q=0.6, dwarven;q=0.5"
-
-        XCTAssertEqual(preferredLanguages.qualityEncoded(), encodedReference)
+        let qualityEncodedReference = "en, fr;q=0.9, jp;q=0.8, quenyan;q=0.7, sindarin;q=0.6, dwarven;q=0.5"
+        XCTAssertEqual(preferredLanguages.qualityEncoded(), qualityEncodedReference)
     }
 }
 
