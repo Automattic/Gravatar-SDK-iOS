0d7187677ac13143b29f1e36da4522ecb4ab0aee
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-09-19 16:01:30 -0500

    Use Constant to define maxPreferredLanguages

--- Sources/Gravatar/Network/LanguagePreferenceProvider.swift
+++ Sources/Gravatar/Network/LanguagePreferenceProvider.swift
@@ -1,12 +1,16 @@
 import Foundation
 
+private enum Constant {
+    static let maxPreferredLanguages = 6
+}
+
 protocol LanguagePreferenceProvider {
     var preferredLanguages: [String] { get }
     var maxPreferredLanguages: Int { get }
 }
 
 struct SystemLanguagePreferenceProvider: LanguagePreferenceProvider {
-    var maxPreferredLanguages = 6
+    let maxPreferredLanguages = Constant.maxPreferredLanguages
     var preferredLanguages: [String] {
         Locale.preferredLanguages
     }
