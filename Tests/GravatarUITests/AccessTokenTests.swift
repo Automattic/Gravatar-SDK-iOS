943376c2468f6cc79435a0159234bc36c9f6d4bf
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-09-24 16:31:54 -0500

    Simplify unit tests

--- Tests/GravatarUITests/AccessTokenTests.swift
+++ Tests/GravatarUITests/AccessTokenTests.swift
@@ -4,19 +4,20 @@ import XCTest
 class AccessTokenTests: XCTestCase {
     func testValidAccessTokenWithAllParameters() {
         // Given a valid callback URL with all parameters
-        let url = url(encodedAccessToken: "abc123")
+        let token = "abc123"
+        let url = URL(string: "https://example.com/callback#access_token=\(token)")!
 
         // When initializing AccessToken from the URL
         let accessToken = AccessToken(from: url)
 
         // Then the access token should be parsed correctly
         XCTAssertNotNil(accessToken)
-        XCTAssertEqual(accessToken?.token, "abc123")
+        XCTAssertEqual(accessToken?.token, token)
     }
 
     func testInvalidAccessTokenWithMissingAccessToken() {
         // Given a callback URL missing the access_token
-        let url = url(encodedAccessToken: nil)
+        let url = URL(string: "https://example.com/callback#foo=bar&baz=qux")!
 
         // When initializing AccessToken from the URL
         let accessToken = AccessToken(from: url)
@@ -26,24 +27,19 @@ class AccessTokenTests: XCTestCase {
     }
 
     func testAccessTokenWithSpecialCharacters() {
-        // Given a callback URL with an access_token containing special characters
-        let url = url(encodedAccessToken: "abc%26123")
+        // Given a callback URL with an access_token containing the literal character `&`
+        // and additional fields separated by the special character `&`
+        let token = "abc&123"
+        let encodedToken = "abc%26123"
+        let url = URL(string: "https://example.com/callback#access_token=\(encodedToken)&baz=qux&foo=bar")!
 
         // When initializing AccessToken from the URL
         let accessToken = AccessToken(from: url)
 
         // Then access_token should correctly decode special characters
         XCTAssertNotNil(accessToken)
-        XCTAssertEqual(accessToken?.token, "abc&123") // `%26` should decode to `&`
+        XCTAssertEqual(accessToken?.token, token) // `%26` should decode to `&`
     }
 
-    private func url(encodedAccessToken: String? = nil) -> URL {
-        var urlString = "https://example.com/callback#"
-
-        if let encodedAccessToken {
-            urlString += "access_token=\(encodedAccessToken)"
-        }
-
-        return URL(string: urlString)!
-    }
+    func testUrlGenerator() {}
 }
