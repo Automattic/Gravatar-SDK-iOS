4d07b68f709d77cf3158381edcc1e521b533c708
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-09-26 12:19:19 -0500

    Test simultaneous fetches only fetch one image from network

--- Tests/GravatarTests/ImageDownloadServiceTests.swift
+++ Tests/GravatarTests/ImageDownloadServiceTests.swift
@@ -116,6 +116,44 @@ final class ImageDownloadServiceTests: XCTestCase {
         XCTAssertEqual(request?.url?.absoluteString, "https://gravatar.com/avatar/HASH")
         XCTAssertNotNil(imageResponse.image)
     }
+
+    func testSimultaneousFetchShouldOnlyTriggerOneNetworkRequest() async throws {
+        let imageURL = URL(string: "https://example.com/image.png")!
+
+        let response = HTTPURLResponse.successResponse(with: imageURL)
+
+        let mockImageData = UIImage(systemName: "iphone.gen2")!.pngData()!
+        let sessionMock = URLSessionMock(returnData: mockImageData, response: response)
+        let cache = TestImageCache()
+        let service = imageDownloadService(with: sessionMock, cache: cache)
+
+        let expectation = XCTestExpectation(description: "First image fetch should complete.")
+
+        // When
+        // Start two simultaneous fetches
+        let fetchTask1 = Task { try await service.fetchImage(with: imageURL, forceRefresh: false) }
+        let fetchTask2 = Task { try await service.fetchImage(with: imageURL, forceRefresh: false) }
+
+        // Then
+        let result1 = try await fetchTask1.value
+        let result2 = try await fetchTask2.value
+
+        expectation.fulfill()
+        await fulfillment(of: [expectation], timeout: 0.5)
+
+        // Assert that both fetches return the same image
+        XCTAssertEqual(result1.image.pngData(), mockImageData)
+        XCTAssertEqual(result2.image.pngData(), mockImageData)
+
+        // Assert that the image was returned twice
+        XCTAssertEqual(cache.messageCount(type: .get), 2)
+
+        // Assert that only one fetch set an `.inProgress` CacheEntry
+        XCTAssertEqual(cache.messageCount(type: .inProgress), 1)
+
+        // Assert that only one fetch set an `.ready` CacheEntry
+        XCTAssertEqual(cache.messageCount(type: .ready), 1)
+    }
 }
 
 private func imageDownloadService(with session: URLSessionProtocol, cache: ImageCaching? = nil) -> ImageDownloadService {
