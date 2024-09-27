05bd7e7eb232ee5cbff54aefe7b8768dab27335d
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-09-26 21:37:23 -0500

    Add unit test to test multiple URL's multiple times, concurrently

--- Tests/GravatarTests/ImageDownloadServiceTests.swift
+++ Tests/GravatarTests/ImageDownloadServiceTests.swift
@@ -104,6 +104,90 @@ final class ImageDownloadServiceTests: XCTestCase {
         // Assert that only one fetch set an `.ready` CacheEntry
         XCTAssertEqual(cache.messageCount(type: .ready, forKey: imageURL.absoluteString), 1)
     }
+
+    func testSimultaneousFetchShouldOnlyTriggerOneNetworkRequestPerUrl() async throws {
+        let imageURL1 = URL(string: "https://example.com/image1.png")!
+        let imageURL2 = URL(string: "https://example.com/image2.png")!
+
+        let mockImageData = UIImage(systemName: "iphone.gen2")!.pngData()!
+
+        let sessionMock = URLSessionMock(
+            returnData: mockImageData,
+            response: HTTPURLResponse()
+        )
+
+        let cache = TestImageCache()
+        let service = imageDownloadService(with: sessionMock, cache: cache)
+
+        let expectation = XCTestExpectation(description: "Image fetches should complete")
+
+        // When
+        // Start simultaneous fetches
+        let fetchTask1 = Task { try await service.fetchImage(with: imageURL1, forceRefresh: false) }
+        let fetchTask2 = Task { try await service.fetchImage(with: imageURL2, forceRefresh: false) }
+        let fetchTask3 = Task { try await service.fetchImage(with: imageURL1, forceRefresh: false) }
+        let fetchTask4 = Task { try await service.fetchImage(with: imageURL2, forceRefresh: false) }
+        let fetchTask5 = Task { try await service.fetchImage(with: imageURL1, forceRefresh: false) }
+        let fetchTask6 = Task { try await service.fetchImage(with: imageURL2, forceRefresh: false) }
+
+        // Then
+        let result1 = try await fetchTask1.value
+        let result2 = try await fetchTask2.value
+        let result3 = try await fetchTask3.value
+        let result4 = try await fetchTask4.value
+        let result5 = try await fetchTask5.value
+        let result6 = try await fetchTask6.value
+
+        expectation.fulfill()
+        await fulfillment(of: [expectation], timeout: 0.5)
+
+        // Assert that all fetches return the same image
+        XCTAssertEqual(result1.image.pngData(), mockImageData)
+        XCTAssertEqual(result2.image.pngData(), mockImageData)
+        XCTAssertEqual(result3.image.pngData(), mockImageData)
+        XCTAssertEqual(result4.image.pngData(), mockImageData)
+        XCTAssertEqual(result5.image.pngData(), mockImageData)
+        XCTAssertEqual(result6.image.pngData(), mockImageData)
+
+        // Assert that all fetches attempted to read from the cache
+        XCTAssertEqual(
+            cache.messageCount(type: .get),
+            6,
+            "All fetches should have attempted to read from the cache"
+        )
+        XCTAssertEqual(
+            cache.messageCount(type: .get, forKey: imageURL1.absoluteString), 3,
+            "All fetches for '\(imageURL1)' should have attempted to read from the cache"
+        )
+        XCTAssertEqual(
+            cache.messageCount(type: .get, forKey: imageURL2.absoluteString), 3,
+            "All fetches for '\(imageURL2)' should have attempted to read from the cache"
+        )
+
+        // Assert that only one fetch set an `.inProgress` CacheEntry
+        XCTAssertEqual(
+            cache.messageCount(type: .inProgress, forKey: imageURL1.absoluteString),
+            1,
+            "Only one fetch for '\(imageURL1)' should have set an `.inProgress` CacheEntry"
+        )
+        XCTAssertEqual(
+            cache.messageCount(type: .inProgress, forKey: imageURL2.absoluteString),
+            1,
+            "Only one fetch for '\(imageURL2)' should have set an `.inProgress` CacheEntry"
+        )
+
+        // Assert that only one fetch set an `.ready` CacheEntry
+        XCTAssertEqual(
+            cache.messageCount(type: .ready, forKey: imageURL1.absoluteString),
+            1,
+            "Only one fetch for '\(imageURL1)' should have set a `.ready` CacheEntry"
+        )
+        XCTAssertEqual(
+            cache.messageCount(type: .ready, forKey: imageURL2.absoluteString),
+            1,
+            "Only one fetch for '\(imageURL2)' should have set a `.ready` CacheEntry"
+        )
+    }
 }
 
 private func imageDownloadService(with session: URLSessionProtocol, cache: ImageCaching? = nil) -> ImageDownloadService {
