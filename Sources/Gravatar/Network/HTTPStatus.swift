01846e7cfd6c7cb707f54e02ffe7bca63a06c97f
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-10-02 21:15:25 -0500

    Update HTTPStatus with 4xx and 5xx statuses

--- Sources/Gravatar/Network/HTTPStatus.swift
+++ Sources/Gravatar/Network/HTTPStatus.swift
@@ -1,7 +1,59 @@
 import Foundation
 
-/// Some HTTP status codes we handle
 package enum HTTPStatus: Int {
-    case notFound = 404
+    // MARK: Client Errors (4xx)
+
+    case badRequest = 400
     case unauthorized = 401
+    case paymentRequired = 402
+    case forbidden = 403
+    case notFound = 404
+    case methodNotAllowed = 405
+    case notAcceptable = 406
+    case proxyAuthenticationRequired = 407
+    case requestTimeout = 408
+    case conflict = 409
+    case gone = 410
+    case lengthRequired = 411
+    case preconditionFailed = 412
+    case payloadTooLarge = 413
+    case uriTooLong = 414
+    case unsupportedMediaType = 415
+    case rangeNotSatisfiable = 416
+    case expectationFailed = 417
+    case iAmATeapot = 418
+    case misdirectedRequest = 421
+    case unprocessableEntity = 422
+    case locked = 423
+    case failedDependency = 424
+    case tooEarly = 425
+    case upgradeRequired = 426
+    case preconditionRequired = 428
+    case tooManyRequests = 429
+    case requestHeaderFieldsTooLarge = 431
+    case unavailableForLegalReasons = 451
+
+    // MARK: - Server Errors (5xx)
+
+    case internalServerError = 500
+    case notImplemented = 501
+    case badGateway = 502
+    case serviceUnavailable = 503
+    case gatewayTimeout = 504
+    case httpVersionNotSupported = 505
+    case variantAlsoNegotiates = 506
+    case insufficientStorage = 507
+    case loopDetected = 508
+    case notExtended = 510
+    case networkAuthenticationRequired = 511
+}
+
+extension HTTPStatus {
+    package var is4xxError: Bool {
+        400 ..< 500 ~= self.rawValue
+    }
+
+    package var is5xxError: Bool {
+        500 ..< 600 ~= self.rawValue
+    }
 }
