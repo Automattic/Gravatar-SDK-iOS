f8e5e74956009a721ac34115fef396a1ef75799e
Pinar Olguc <pinarolguc@gmail.com>
2024-10-03 15:07:31 +0300

    Add Info.swift

--- Sources/Gravatar/Info.swift
+++ Sources/Gravatar/Info.swift
@@ -1,8 +1,22 @@
-//
-//  File.swift
-//  
-//
-//  Created by Pinar Olguc on 3.10.2024.
-//
-
 import Foundation
+
+struct Info {
+    public static var sdkVersion: String? {
+        getInfoValue(forKey: "CFBundleShortVersionString") as? String
+    }
+
+    public static var appName: String? {
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+
+    private static func getInfoValue(forKey key: String) -> Any? {
+        // Access the SDKInfo.plist using Bundle.module
+        guard let url = Bundle.module.url(forResource: "SDKInfo", withExtension: "plist"),
+              let data = try? Data(contentsOf: url),
+              let plist = try? PropertyListSerialization.propertyList(from: data, options: [], format: nil) as? [String: Any]
+        else {
+            return nil
+        }
+        return plist[key]
+    }
+}
