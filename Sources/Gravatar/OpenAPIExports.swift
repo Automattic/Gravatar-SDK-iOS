68fd36e79e2fa781827ab012e71baef1209d9086
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-10-04 17:18:05 -0500

    Export OpenAPI types

---
+++ Sources/Gravatar/OpenAPIExports.swift
@@ -0,0 +1,23 @@
+import OpenAPIClient
+
+// MARK: OpenAPI Models: Public
+
+public typealias CryptoWalletAddress = OpenAPIClient.CryptoWalletAddress
+public typealias GalleryImage = OpenAPIClient.GalleryImage
+public typealias Interest = OpenAPIClient.Interest
+public typealias Language = OpenAPIClient.Language
+public typealias Link = OpenAPIClient.Link
+public typealias Profile = OpenAPIClient.Profile
+public typealias ProfileContactInfo = OpenAPIClient.ProfileContactInfo
+public typealias ProfilePayments = OpenAPIClient.ProfilePayments
+public typealias VerifiedAccount = OpenAPIClient.VerifiedAccount
+
+// MARK: - OpenAPI Models: Package
+
+package typealias Avatar = OpenAPIClient.Avatar
+
+// MARK: - OpenAPI Models: Internal
+
+typealias AssociatedResponse = OpenAPIClient.AssociatedResponse
+typealias ModelError = OpenAPIClient.ModelError
+typealias SetEmailAvatarRequest = OpenAPIClient.SetEmailAvatarRequest
