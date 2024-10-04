a02e7ee8c9c48379979fd49d81b4f2a005317f01
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-10-04 13:18:41 +0200

    Making some openapi structs internal

--- Sources/Gravatar/OpenApi/Generated/SetEmailAvatarRequest.swift
+++ Sources/Gravatar/OpenApi/Generated/SetEmailAvatarRequest.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-public struct SetEmailAvatarRequest: Codable, Hashable, Sendable {
+struct SetEmailAvatarRequest: Codable, Hashable, Sendable {
     /// The email SHA256 hash to set the avatar for.
-    public private(set) var emailHash: String
+    private(set) var emailHash: String
 
     enum InternalCodingKeys: String, CodingKey, CaseIterable {
         case emailHash = "email_hash"
@@ -10,14 +10,14 @@ public struct SetEmailAvatarRequest: Codable, Hashable, Sendable {
 
     // Encodable protocol methods
 
-    public func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: InternalCodingKeys.self)
         try container.encode(emailHash, forKey: .emailHash)
     }
 
     // Decodable protocol methods
 
-    public init(from decoder: Decoder) throws {
+    init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: InternalCodingKeys.self)
 
         emailHash = try container.decode(String.self, forKey: .emailHash)
