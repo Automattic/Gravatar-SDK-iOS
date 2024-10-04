2e1a915ceb7e05781c76c32a82725e0c9114e2cd
Pinar Olguc <pinarolguc@gmail.com>
2024-10-04 10:29:15 +0300

    Update code comment

--- Sources/Gravatar/Bundle+ResourceBundle.swift
+++ Sources/Gravatar/Bundle+ResourceBundle.swift
@@ -5,7 +5,10 @@ private class BundleFinder: NSObject {}
 extension Bundle {
     static var module: Bundle {
         let defaultBundle = Bundle(for: BundleFinder.self)
-        // If installed with CocoaPods, resources will be in GravatarUI.bundle
+        // If installed with CocoaPods, resources will be in Gravatar.bundle
+        // The name of the bundle "Gravatar.bundle" (without the .bundle file extension)
+        // needs to match the key in the respective Gravatar.podspec:
+        // `s.resource_bundles = { 'Gravatar' => ['Sources/Gravatar/Resources/*.plist'] }`
         if let bundleURL = defaultBundle.resourceURL,
            let resourceBundle = Bundle(url: bundleURL.appendingPathComponent("Gravatar.bundle"))
         {
