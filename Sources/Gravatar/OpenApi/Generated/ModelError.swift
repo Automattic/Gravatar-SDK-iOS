2e787c5e45a52cce1efbd8311a4409c209aec556
Eduardo Toledo <eduardo.toledo@automattic.com>
2024-10-08 14:53:57 +0200

    Remove open-api deprecations

--- Sources/Gravatar/OpenApi/Generated/ModelError.swift
+++ Sources/Gravatar/OpenApi/Generated/ModelError.swift
@@ -8,19 +8,12 @@ struct ModelError: Codable, Hashable, Sendable {
     /// The error code for the error message
     private(set) var code: String?
 
-    @available(*, deprecated, message: "init will become internal on the next release")
     init(error: String, code: String? = nil) {
         self.error = error
         self.code = code
     }
 
-    @available(*, deprecated, message: "CodingKeys will become internal on the next release.")
     enum CodingKeys: String, CodingKey, CaseIterable {
-        case error
-        case code
-    }
-
-    enum InternalCodingKeys: String, CodingKey, CaseIterable {
         case error
         case code
     }
@@ -28,17 +21,8 @@ struct ModelError: Codable, Hashable, Sendable {
     // Encodable protocol methods
 
     func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: InternalCodingKeys.self)
+        var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(error, forKey: .error)
         try container.encodeIfPresent(code, forKey: .code)
-    }
-
-    // Decodable protocol methods
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: InternalCodingKeys.self)
-
-        error = try container.decode(String.self, forKey: .error)
-        code = try container.decodeIfPresent(String.self, forKey: .code)
     }
 }
