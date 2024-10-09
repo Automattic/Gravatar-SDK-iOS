93f7c01bfb92eeef804c8a47f81bff4f31e4bf8e
Pinar Olguc <pinarolguc@gmail.com>
2024-10-09 16:55:16 +0300

    Remove unused URLSessionProtocol method

--- Sources/Gravatar/Network/URLSessionProtocol.swift
+++ Sources/Gravatar/Network/URLSessionProtocol.swift
@@ -2,10 +2,6 @@ import UIKit
 
 /// Protocol for dependency injection purposes. `URLSession` conforms to  this protocol.
 public protocol URLSessionProtocol: Sendable {
-    func dataTask(
-        with request: URLRequest,
-        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
-    ) -> URLSessionDataTask
 
     func data(for request: URLRequest) async throws -> (Data, URLResponse)
 
