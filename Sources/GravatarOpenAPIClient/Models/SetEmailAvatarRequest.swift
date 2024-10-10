e1e7191e65253ef5eda6550a677c17360fc4b404
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-10-10 12:41:17 -0500

    Regenerate SetEmailAvatarRequest

--- Sources/GravatarOpenAPIClient/Models/SetEmailAvatarRequest.swift
+++ Sources/GravatarOpenAPIClient/Models/SetEmailAvatarRequest.swift
@@ -4,32 +4,18 @@ public struct SetEmailAvatarRequest: Codable, Hashable, Sendable {
     /// The email SHA256 hash to set the avatar for.
     public private(set) var emailHash: String
 
-    @available(*, deprecated, message: "init will become internal on the next release")
-    public init(emailHash: String) {
+    init(emailHash: String) {
         self.emailHash = emailHash
     }
 
-    @available(*, deprecated, message: "CodingKeys will become internal on the next release.")
-    public enum CodingKeys: String, CodingKey, CaseIterable {
-        case emailHash = "email_hash"
-    }
-
-    enum InternalCodingKeys: String, CodingKey, CaseIterable {
+    enum CodingKeys: String, CodingKey, CaseIterable {
         case emailHash = "email_hash"
     }
 
     // Encodable protocol methods
 
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: InternalCodingKeys.self)
+        var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(emailHash, forKey: .emailHash)
-    }
-
-    // Decodable protocol methods
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: InternalCodingKeys.self)
-
-        emailHash = try container.decode(String.self, forKey: .emailHash)
     }
 }
