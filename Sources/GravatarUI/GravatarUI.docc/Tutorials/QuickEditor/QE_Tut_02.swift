868e1f7c0aa6bdab053b142defc4fc01593cdbe3
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-10-15 17:15:13 -0500

    Add a simple tutorial for using the Quick Editor from a SwiftUI View

---
+++ Sources/GravatarUI/GravatarUI.docc/Tutorials/QuickEditor/QE_Tut_02.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("pickerEmail") private var email: String = ""
+    @AppStorage("pickAuthToken") private var authToken: String = ""
+    @State private var isPresentingPicker: Bool = false
+    
+    var body: some View {
+        VStack {
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .font(.callout)
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.emailAddress)
+                    .disableAutocorrection(true)
+                SecureField("Enter your auth token", text: $authToken)
+                    .font(.callout)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+            }
+            .padding(20)
+            
+            Button("Show Quick Editor") {
+                isPresentingPicker = true
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
