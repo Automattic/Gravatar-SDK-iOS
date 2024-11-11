01405202b05730b1608ec0cabcc94ebb27417773
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-11-11 16:52:21 -0600

    Assert that the image is square before uploading

--- Sources/Gravatar/Network/Services/ImageUploadService.swift
+++ Sources/Gravatar/Network/Services/ImageUploadService.swift
@@ -18,6 +18,11 @@ struct ImageUploadService: ImageUploader {
         avatarSelection: AvatarSelection = .preserveSelection,
         additionalHTTPHeaders: [HTTPHeaderField]?
     ) async throws -> (data: Data, response: HTTPURLResponse) {
+        assert(
+            image.isSquare(),
+            "This image is not square, and uploading it will fail. Consider using the default `cropToFitThreshold` when calling `AvatarService.upload(::::)`."
+        )
+
         guard let data: Data = {
             if #available(iOS 17.0, *) {
                 image.heicData()
