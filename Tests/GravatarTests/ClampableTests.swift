b9551e6efca6a3ec36facdd82800d49ed694b7c7
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-11-14 21:39:11 -0600

    Split up and name Clampable tests

--- Tests/GravatarTests/ClampableTests.swift
+++ Tests/GravatarTests/ClampableTests.swift
@@ -4,23 +4,26 @@ import Testing
 @testable import Gravatar
 
 struct ClampableTests {
-    @Test
-    func testCGFloatOutOfRangeDoesClamp() async throws {
-        // Test positive ranges
+    @Test("CGFloat outside of positive range does clamp")
+    func cgFloatOutOfPositiveRangeDoesClamp() async throws {
         #expect(CGFloat(2.0).clamped(to: 0.0 ... 1.0) == 1.0)
         #expect(CGFloat(1_000_000.0).clamped(to: 0.0 ... 1.0) == 1.0)
         #expect(CGFloat(-2.0).clamped(to: 0.0 ... 1.0) == 0.0)
         #expect(CGFloat(-1_000_000.0).clamped(to: 0.0 ... 1.0) == 0.0)
         #expect(CGFloat(1.0001).clamped(to: 0.0 ... 1.0) == 1.0)
+    }
 
-        // Test negative ranges
+    @Test("CGFloat outside of negative range does clamp")
+    func cgFloatOutOfNegativeRangeDoesClamp() async throws {
         #expect(CGFloat(2.0).clamped(to: -1.0 ... 0.0) == 0.0)
         #expect(CGFloat(1_000_000.0).clamped(to: -1.0 ... 0.0) == 0.0)
         #expect(CGFloat(-2.0).clamped(to: -1.0 ... 0.0) == -1.0)
         #expect(CGFloat(-1_000_000.0).clamped(to: -1.0 ... 0.0) == -1.0)
         #expect(CGFloat(0.0001).clamped(to: -1.0 ... 0.0) == 0.0)
+    }
 
-        // Test unusaul numbers
+    @Test("Unusual CGFloat does clamp")
+    func unusualNumbersDoClamp() async throws {
         #expect(CGFloat.infinity.clamped(to: 0.0 ... 1.0) == 1.0)
         #expect(CGFloat.greatestFiniteMagnitude.clamped(to: 0.0 ... 1.0) == 1.0)
         #expect(CGFloat.leastNonzeroMagnitude.clamped(to: 1.0 ... 2.0) == 1.0)
@@ -31,7 +34,7 @@ struct ClampableTests {
         #expect(CGFloat.nan.clamped(to: 1.0 ... 2.0) == 1.0)
     }
 
-    @Test
+    @Test("CGFloat within range does not clamp")
     func testCGFloatInRangeDoesNotClamp() async throws {
         #expect(CGFloat(1.0).clamped(to: 0.0 ... 1.0) == 1.0)
         #expect(CGFloat(0.5).clamped(to: 0.0 ... 1.0) == 0.5)
