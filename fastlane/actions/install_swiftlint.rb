a24399f3ce0d81beca9b894f2bf3293d93ce5e1c
Andrew Montgomery <andrewdmontgomery@mac.com>
2024-12-20 17:46:26 -0600

    Refactor more

--- fastlane/actions/install_swiftlint.rb
+++ fastlane/actions/install_swiftlint.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require 'fileutils'
-require 'open3'
-require 'open-uri'
+require 'net/http'
+require 'json'
 require 'zip'
+require 'tmpdir'
 
 module Fastlane
   module Actions
@@ -23,23 +24,20 @@ module Fastlane
         install_swiftlint(zip_file, install_binary_path)
       end
 
-      # Prepares paths and returns key variables
       def self.prepare_paths(params)
         swiftlint_version = params[:version]
         install_dir = params[:install_path]
 
         bin_dir = File.join(install_dir, 'bin')
         cache_dir = File.join(install_dir, 'cache')
-        temp_dir = '/tmp/swiftlint_install'
         install_binary_path = File.join(bin_dir, 'swiftlint')
 
-        download_version = swiftlint_version == 'latest' ? fetch_latest_release_tag : swiftlint_version
+        download_version = swiftlint_version == 'latest' ? fetch_latest_release_tag : validate_version(swiftlint_version)
         zip_file = File.join(cache_dir, "portable_swiftlint_#{download_version}.zip")
 
         # Ensure directories exist
         FileUtils.mkdir_p(bin_dir)
         FileUtils.mkdir_p(cache_dir)
-        FileUtils.mkdir_p(temp_dir)
 
         [download_version, install_binary_path, zip_file]
       end
@@ -61,68 +59,97 @@ module Fastlane
         end
       end
 
-      # Downloads SwiftLint if not cached
       def self.download_swiftlint_if_needed(version, zip_file)
         if File.exist?(zip_file)
-          UI.message("Using cached SwiftLint version #{version}.")
+          UI.message("Using cached SwiftLint version #{version}")
         else
           UI.message("Downloading SwiftLint version #{version}...")
-          download_swiftlint(version, zip_file)
+          url = "https://github.com/realm/SwiftLint/releases/download/#{version}/portable_swiftlint.zip"
+          download_file(url, zip_file)
         end
       end
 
-      def self.download_swiftlint(version, destination)
-        url = "https://github.com/realm/SwiftLint/releases/download/#{version}/portable_swiftlint.zip"
+      def self.download_file(url, destination)
+        uri = URI.parse(url)
 
-        URI.parse(url).open do |download|
-          File.binwrite(destination, download.read)
-        end
+        response = fetch_http_response(uri)
+        write_to_file(response, destination)
       rescue StandardError => e
         UI.user_error!("Failed to download SwiftLint: #{e.message}")
       end
 
-      def self.install_swiftlint(zip_file, install_binary_path)
-        UI.message('Installing SwiftLint...')
-        temp_dir = '/tmp/swiftlint_install'
+      # rubocop:disable Metrics/AbcSize
+      # Fetch the HTTP response, following redirects if needed
+      def self.fetch_http_response(uri)
+        loop do
+          response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+            request = Net::HTTP::Get.new(uri)
+            http.request(request)
+          end
 
-        # Extract the zip file
-        Zip::File.open(zip_file) do |zip|
-          zip.each do |entry|
-            target_path = File.join(temp_dir, entry.name)
-            begin
-              entry.extract(target_path) { true }
-            rescue StandardError
-              UI.user_error!("Failed to extract #{entry.name}")
-            end
+          case response
+          when Net::HTTPSuccess
+            return response
+          when Net::HTTPRedirection
+            new_location = response['location']
+            uri = URI.parse(new_location)
+          when Net::HTTPTooManyRequests
+            retry_after = response['Retry-After']&.to_i || 5
+            UI.message("Rate limited. Retrying after #{retry_after} seconds...")
+            sleep(retry_after)
+          else
+            raise "Failed to fetch SwiftLint from #{uri}: #{response.code} #{response.message}"
           end
         end
+      end
+      # rubocop:enable Metrics/AbcSize
 
-        # Move the binary to the install path
-        swiftlint_binary = File.join(temp_dir, 'swiftlint')
-        FileUtils.mv(swiftlint_binary, install_binary_path)
-        FileUtils.chmod('+x', install_binary_path)
-        UI.success("SwiftLint installed successfully at #{install_binary_path}")
+      # Write the HTTP response body to a file
+      def self.write_to_file(response, destination)
+        File.binwrite(destination, response.body)
+        UI.success("Downloaded SwiftLint to #{destination}")
+      end
+
+      def self.install_swiftlint(zip_file, install_binary_path)
+        temp_dir = Dir.mktmpdir('swiftlint_install')
+        begin
+          UI.message('Installing SwiftLint...')
+          # Extract the zip file
+          Zip::File.open(zip_file) do |zip|
+            zip.each do |entry|
+              entry.extract(File.join(temp_dir, entry.name)) { true }
+            end
+          end
+
+          # Move the binary to the install path
+          swiftlint_binary = File.join(temp_dir, 'swiftlint')
+          FileUtils.mv(swiftlint_binary, install_binary_path)
+          FileUtils.chmod('+x', install_binary_path)
+          UI.success("SwiftLint installed successfully at #{install_binary_path}")
+        ensure
+          FileUtils.rm_rf(temp_dir) # Clean up temp directory
+        end
       rescue StandardError => e
         UI.user_error!("Failed to install SwiftLint: #{e.message}")
-      ensure
-        FileUtils.rm_rf(temp_dir) # Clean up temp directory
       end
 
       def self.fetch_latest_release_tag
         UI.message('Fetching the latest SwiftLint version...')
-        api_url = 'https://api.github.com/repos/realm/SwiftLint/releases/latest'
-        latest_version = nil
+        api_url = URI('https://api.github.com/repos/realm/SwiftLint/releases/latest')
 
-        begin
-          response = URI.parse(api_url).open.read
-          json = JSON.parse(response)
-          latest_version = json['tag_name']
-        rescue StandardError => e
-          UI.user_error!("Failed to fetch the latest SwiftLint version: #{e.message}")
-        end
+        response = Net::HTTP.get(api_url)
+        json = JSON.parse(response)
+        latest_version = json['tag_name']
 
         UI.message("Latest SwiftLint version: #{latest_version}")
         latest_version
+      rescue StandardError => e
+        UI.user_error!("Failed to fetch the latest SwiftLint version: #{e.message}")
+      end
+
+      def self.validate_version(version)
+        UI.user_error!("Invalid version format: #{version}. Use 'latest' or a valid semantic version (e.g., '0.51.0').") unless version.match?(/^\d+\.\d+\.\d+$/)
+        version
       end
 
       # Fastlane metadata
