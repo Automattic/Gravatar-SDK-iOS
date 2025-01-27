4d3a9789e3c1bf26dbc63d0199292fec1089f541
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-01-27 13:55:23 -0600

    Revert "Remove unused custom headers"
    This reverts commit a3218c02744270583526211f2e92c11802e3e5d1.

--- Sources/Gravatar/Network/Services/URLSessionHTTPClient.swift
+++ Sources/Gravatar/Network/Services/URLSessionHTTPClient.swift
@@ -18,6 +18,9 @@ struct URLSessionHTTPClient: HTTPClient {
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = [
             "Accept": "application/json",
+            "X-Platform": "ios",
+            "X-SDK-Version": BundleInfo.sdkVersion ?? "",
+            "X-Source": BundleInfo.appName ?? "",
             "User-Agent": Self.userAgent,
         ]
         self.urlSession = urlSession ?? URLSession(configuration: configuration)
