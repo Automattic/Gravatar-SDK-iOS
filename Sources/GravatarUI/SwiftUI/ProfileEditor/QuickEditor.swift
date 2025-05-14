130329a5b99e67267ae73e5282028a45e1da353d
Eduardo Toledo <etoledom@icloud.com>
2025-05-14 12:26:27 +0200

    fix `onChange(of:)` usage

--- Sources/GravatarUI/SwiftUI/ProfileEditor/QuickEditor.swift
+++ Sources/GravatarUI/SwiftUI/ProfileEditor/QuickEditor.swift
@@ -145,8 +145,8 @@ struct QuickEditor<ImageEditor: ImageEditorView>: View {
         .onChange(of: model.hasUnsavedChanges) { _ in
             unsavedChangesAlertPresentationModel.hasUnsavedChanges = model.hasUnsavedChanges
         }
-        .onChange(of: dismissAttempt) { _ in
-            guard dismissAttempt else { return }
+        .onChange(of: dismissAttempt) { newValue in
+            guard newValue else { return }
             if unsavedChangesAlertPresentationModel.hasUnsavedChanges {
                 unsavedChangesAlertPresentationModel.presentAlert = true
             }
