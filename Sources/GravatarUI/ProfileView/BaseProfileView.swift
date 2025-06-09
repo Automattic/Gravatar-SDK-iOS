0cc8b52ea03d0375cd6d1544b6c0b7486e4867c8
Eduardo Toledo <etoledom@icloud.com>
2025-06-09 11:42:52 +0200

    Using avatar border color from palette instead than from avatar config (which is deprecated)

--- Sources/GravatarUI/ProfileView/BaseProfileView.swift
+++ Sources/GravatarUI/ProfileView/BaseProfileView.swift
@@ -410,7 +410,7 @@ open class BaseProfileView: UIView, UIContentView {
         if let avatarProvider = avatarProvider as? DefaultAvatarProvider {
             avatarProvider.cornerRadiusCalculator = config.avatarConfiguration.cornerRadiusCalculator
             avatarProvider.avatarBorderWidth = config.avatarConfiguration.borderWidth
-            avatarProvider.avatarBorderColor = config.avatarConfiguration.borderColor
+            avatarProvider.avatarBorderColor = config.palette.palette.border
         }
         if let length = config.avatarConfiguration.avatarLength {
             avatarLength = length
