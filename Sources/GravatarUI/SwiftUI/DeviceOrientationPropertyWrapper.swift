de619619b02877316588827e89d5f3ef1c54673a
Eduardo Toledo <etoledom@icloud.com>
2025-06-13 19:03:42 +0200

    Removing Profile card for device in landscape while showing keyboard

---
+++ Sources/GravatarUI/SwiftUI/DeviceOrientationPropertyWrapper.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import Combine
+
+enum Orientation {
+    case landscape
+    case portrait
+    case unknown
+}
+
+@propertyWrapper struct DeviceOrientation: DynamicProperty {
+    @StateObject private var manager = DeviceOrientationManager()
+
+    var wrappedValue: Orientation {
+        manager.orientation
+    }
+}
+
+private class DeviceOrientationManager: ObservableObject {
+    @Published var orientation: Orientation = .unknown
+
+    private var cancellables: Set<AnyCancellable> = []
+
+    @MainActor
+    init() {
+        orientation = UIDevice.current.orientation.map()
+
+        NotificationCenter.default
+            .publisher(for: UIDevice.orientationDidChangeNotification)
+            .sink() { [weak self] _ in
+                self?.orientation = UIDevice.current.orientation.map()
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension UIDeviceOrientation {
+    fileprivate func map() -> Orientation {
+        switch self {
+        case .portrait, .portraitUpsideDown:
+            return .portrait
+        case .landscapeLeft, .landscapeRight:
+            return .landscape
+        default:
+            return .unknown
+        }
+    }
+}
