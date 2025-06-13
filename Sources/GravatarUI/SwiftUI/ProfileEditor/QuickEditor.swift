f3df99af3f5b0666e403e97c642fa86713f4549e
Eduardo Toledo <etoledom@icloud.com>
2025-06-13 16:26:38 +0200

    Remove Profile card for big accessibility text sizes
    Only with the keyboard present

--- Sources/GravatarUI/SwiftUI/ProfileEditor/QuickEditor.swift
+++ Sources/GravatarUI/SwiftUI/ProfileEditor/QuickEditor.swift
@@ -29,6 +29,7 @@ struct QuickEditor<ImageEditor: ImageEditorView>: View {
     @Environment(\.oauthSession) private var oauthSession
     @Environment(\.colorScheme) var colorScheme: ColorScheme
     @Environment(\.verticalSizeClass) var vertcalSizeClass
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
 
     @AppStorage("QuickEditor.startOAuthOnAppear") private var startOAuthOnAppear: Bool = false
     @State private var fetchedToken: String?
@@ -230,7 +231,11 @@ struct QuickEditor<ImageEditor: ImageEditorView>: View {
         let screenHeight = UIScreen.main.bounds.height
         let iPhoneSE3rdGenScreenHeight: CGFloat = 667
 
-        return (vertcalSizeClass == .compact || screenHeight <= iPhoneSE3rdGenScreenHeight) && model.isKeyboardPresented
+        return model.isKeyboardPresented && (
+            vertcalSizeClass == .compact ||
+            screenHeight <= iPhoneSE3rdGenScreenHeight ||
+            dynamicTypeSize >= .accessibility3
+        )
     }
 
     @ViewBuilder
@@ -241,6 +246,7 @@ struct QuickEditor<ImageEditor: ImageEditorView>: View {
                     .accumulateIntrinsicHeight()
             }
             profileView()
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
                 .accumulateIntrinsicHeight()
         } else {
             EmptyView()
